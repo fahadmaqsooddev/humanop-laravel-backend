4d7da3273a1ab96e0d73e5989bc70e8613157c7d
aamir <m.ahtasham@eye4tech.com>
2024-05-09 12:23:33 +0500

    videos upload

--- resources/views/client-dashboard/video.blade.php
+++ resources/views/client-dashboard/video.blade.php
@@ -1,13 +1,14 @@
 @extends('user_type.auth', ['parentFolder' => 'dashboards', 'childFolder' => 'virtual'])
 
 @section('content')
-  <main class="main-content mt-1 border-radius-lg">
-    <div class="section min-vh-85 position-relative transform-scale-0 transform-scale-md-7">
-      <div class="container-fluid">
-        <div class="row">
-            <iframe width="500" height="515" src="https://www.youtube.com/embed/vQChW_jgMMM"></iframe>
+    <main class="main-content mt-1 border-radius-lg">
+        <div class="section min-vh-85 position-relative transform-scale-0 transform-scale-md-7">
+            <div class="container-fluid">
+                <div class="row">
+                    <iframe width="500" height="515" src="{{asset('assets/video/Intro to Communication Style.mp4')}} "
+                            allowfullscreen frameborder="0"></iframe>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </main>
+    </main>
 @endsection
