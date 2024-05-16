45523a9c98d3b97663686edb5625027150fbc389
hafizirfan987 <muhammad.irfan@eye4tech.com>
2024-05-16 12:33:00 +0500

    redesing login and register page

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <main class="main-content  mt-0">
-        <div class="page-header align-items-start section-height-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('assets/img/curved-images/curved6.jpg');">
-            <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="page-header align-items-start section-height-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('assets/img/login.webp');">
+            {{-- <span class="mask bg-gradient-dark opacity-6"></span> --}}
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 text-center mx-auto">
@@ -20,75 +20,72 @@
                 <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                     <div class="card z-index-0">
                         <div class="card-header text-center pt-4">
-                            <h5>Register with</h5>
+                            <h5 class="text-white">Register with</h5>
                         </div>
-                        <div class="row px-xl-5 px-sm-4 px-3">
-                            <div class="col-3 ms-auto px-1">
-                                <a class="btn btn-outline-light w-100" href="javascript:;">
-                                    <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink32">
-                                        <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <g id="facebook-3" transform="translate(3.000000, 3.000000)" fill-rule="nonzero">
-                                                <circle id="Oval" fill="#3C5A9A" cx="29.5091719" cy="29.4927506" r="29.4882047"></circle>
-                                                <path d="M39.0974944,9.05587273 L32.5651312,9.05587273 C28.6886088,9.05587273 24.3768224,10.6862851 24.3768224,16.3054653 C24.395747,18.2634019 24.3768224,20.1385313 24.3768224,22.2488655 L19.8922122,22.2488655 L19.8922122,29.3852113 L24.5156022,29.3852113 L24.5156022,49.9295284 L33.0113092,49.9295284 L33.0113092,29.2496356 L38.6187742,29.2496356 L39.1261316,22.2288395 L32.8649196,22.2288395 C32.8649196,22.2288395 32.8789377,19.1056932 32.8649196,18.1987181 C32.8649196,15.9781412 35.1755132,16.1053059 35.3144932,16.1053059 C36.4140178,16.1053059 38.5518876,16.1085101 39.1006986,16.1053059 L39.1006986,9.05587273 L39.0974944,9.05587273 L39.0974944,9.05587273 Z" id="Path" fill="#FFFFFF"></path>
-                                            </g>
-                                        </g>
-                                    </svg>
+                        <div class="css-mgpqwz">
+                            <div class="d-flex justify-content-center" style="margin-right: 20px;">
+                                <a href="#">
+                                    <button class="btn btn-primary p-2 border border-radius-lg border-gray-800" type="button" style="background-color: rgb(19, 21, 54);">
+                                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" 
+                                             class="material-icons-round notranslate" aria-hidden="true" height="40px" width="40px" 
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z">
+                                            </path>
+                                        </svg>
+                                    </button>
                                 </a>
                             </div>
-                            <div class="col-3 px-1">
-                                <a class="btn btn-outline-light w-100" href="javascript:;">
-                                    <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                        <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <g id="apple-black" transform="translate(7.000000, 0.564551)" fill="#000000" fill-rule="nonzero">
-                                                <path d="M40.9233048,32.8428307 C41.0078713,42.0741676 48.9124247,45.146088 49,45.1851909 C48.9331634,45.4017274 47.7369821,49.5628653 44.835501,53.8610269 C42.3271952,57.5771105 39.7241148,61.2793611 35.6233362,61.356042 C31.5939073,61.431307 30.2982233,58.9340578 25.6914424,58.9340578 C21.0860585,58.9340578 19.6464932,61.27947 15.8321878,61.4314159 C11.8738936,61.5833617 8.85958554,57.4131833 6.33064852,53.7107148 C1.16284874,46.1373849 -2.78641926,32.3103122 2.51645059,22.9768066 C5.15080028,18.3417501 9.85858819,15.4066355 14.9684701,15.3313705 C18.8554146,15.2562145 22.5241194,17.9820905 24.9003639,17.9820905 C27.275104,17.9820905 31.733383,14.7039812 36.4203248,15.1854154 C38.3824403,15.2681959 43.8902255,15.9888223 47.4267616,21.2362369 C47.1417927,21.4153043 40.8549638,25.1251794 40.9233048,32.8428307 M33.3504628,10.1750144 C35.4519466,7.59650964 36.8663676,4.00699306 36.4804992,0.435448578 C33.4513624,0.558856931 29.7884601,2.48154382 27.6157341,5.05863265 C25.6685547,7.34076135 23.9632549,10.9934525 24.4233742,14.4943068 C27.7996959,14.7590956 31.2488715,12.7551531 33.3504628,10.1750144" id="Shape"></path>
-                                            </g>
-                                        </g>
-                                    </svg>
+                            <div class="d-flex justify-content-center " style="margin-right: 20px;>
+                                <a href="#">
+                                    <button class="btn btn-primary p-2 border border-radius-lg border-gray-800" type="button" style="background-color: rgb(19, 21, 54);">
+                                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" aria-hidden="true"
+                                             height="40px" width="40px" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z">
+                                            </path>
+                                        </svg>
+                                    </button>
                                 </a>
                             </div>
-                            <div class="col-3 me-auto px-1">
-                                <a class="btn btn-outline-light w-100" href="javascript:;">
-                                    <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                        <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <g id="google-icon" transform="translate(3.000000, 2.000000)" fill-rule="nonzero">
-                                                <path d="M57.8123233,30.1515267 C57.8123233,27.7263183 57.6155321,25.9565533 57.1896408,24.1212666 L29.4960833,24.1212666 L29.4960833,35.0674653 L45.7515771,35.0674653 C45.4239683,37.7877475 43.6542033,41.8844383 39.7213169,44.6372555 L39.6661883,45.0037254 L48.4223791,51.7870338 L49.0290201,51.8475849 C54.6004021,46.7020943 57.8123233,39.1313952 57.8123233,30.1515267" id="Path" fill="#4285F4"></path>
-                                                <path d="M29.4960833,58.9921667 C37.4599129,58.9921667 44.1456164,56.3701671 49.0290201,51.8475849 L39.7213169,44.6372555 C37.2305867,46.3742596 33.887622,47.5868638 29.4960833,47.5868638 C21.6960582,47.5868638 15.0758763,42.4415991 12.7159637,35.3297782 L12.3700541,35.3591501 L3.26524241,42.4054492 L3.14617358,42.736447 C7.9965904,52.3717589 17.959737,58.9921667 29.4960833,58.9921667" id="Path" fill="#34A853"></path>
-                                                <path d="M12.7159637,35.3297782 C12.0932812,33.4944915 11.7329116,31.5279353 11.7329116,29.4960833 C11.7329116,27.4640054 12.0932812,25.4976752 12.6832029,23.6623884 L12.6667095,23.2715173 L3.44779955,16.1120237 L3.14617358,16.2554937 C1.14708246,20.2539019 0,24.7439491 0,29.4960833 C0,34.2482175 1.14708246,38.7380388 3.14617358,42.736447 L12.7159637,35.3297782" id="Path" fill="#FBBC05"></path>
-                                                <path d="M29.4960833,11.4050769 C35.0347044,11.4050769 38.7707997,13.7975244 40.9011602,15.7968415 L49.2255853,7.66898166 C44.1130815,2.91684746 37.4599129,0 29.4960833,0 C17.959737,0 7.9965904,6.62018183 3.14617358,16.2554937 L12.6832029,23.6623884 C15.0758763,16.5505675 21.6960582,11.4050769 29.4960833,11.4050769" id="Path" fill="#EB4335"></path>
-                                            </g>
-                                        </g>
-                                    </svg>
+                            <div class="d-flex justify-content-center" style="margin-right: 20px;>
+                                <a href="#">
+                                    <button class="btn btn-primary  p-2 border border-radius-lg border-gray-800" type="button" style="background-color: rgb(19, 21, 54);">
+                                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 488 512" aria-hidden="true"
+                                             height="40px" width="40px" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
+                                            </path>
+                                        </svg>
+                                    </button>
                                 </a>
-                            </div>
-                            <div class="mt-2 position-relative text-center">
-                                <p class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
-                                    or
-                                </p>
                             </div>
                         </div>
+                    <p class="text-center text-white">or</p>
                         <div class="card-body">
                             <form action="/register" method="POST" role="form text-start">
                                 @csrf
                                 <div>
-                                    <div class=" ">
-                                        <input type="text" class="form-control " placeholder="Name" aria-label="Name" aria-describedby="email-addon" name="name" id="name" value="{{ old('name') }}" required>
-                                        @error('name')
-                                            <p class="text-danger text-xs mt-2 mb-2">{{ $message }}</p>
-                                        @enderror
+                                    <div class="input-group">
+                                        <input type="text" class="form-control " placeholder="Name" aria-label="Name" aria-describedby="email-addon" name="name" id="name" value="{{ old('name') }}" required style="background-color: #0F1535; color: white; border-radius: 15px;">
                                     </div>
+                                    @error('name')
+                                        <p class="text-danger text-xs mt-2 mb-2">{{ $message }}</p>
+                                    @enderror
                                 </div>
+                                
                                 <div class="mt-3">
                                     <div class=" ">
-                                        <input type="email" class="form-control " placeholder="Email" aria-label="Email" aria-describedby="email-addon" name="email" id="email" value="{{ old('email') }}" required>
+                                        <input type="email" class="form-control " placeholder="Email" aria-label="Email" aria-describedby="email-addon" name="email" id="email" value="{{ old('email') }}" required style="background-color: #0F1535; color: white; border-radius: 15px;">
                                         @error('email')
                                             <p class="text-danger text-xs mt-2 mb-2">{{ $message }}</p>
                                         @enderror
                                     </div>
                                 </div>
                                 <div class="mt-3">
-                                    <div class=" ">
-                                        <select class="form-control" name="role_id" id="role_id" name="user_type">
-                                            <option value="" selected hidden>Choose</option>
+                                    <div class="">
+                                        <select class="form-control" name="role_id" id="role_id" name="user_type" style="background-color: #0F1535; color: white; border-radius: 15px;">
+                                            <option value=""  selected hidden >Choose</option>
                                             <option value="1">{{ __('Admin') }}</option>
                                             <option value="2">{{ __('Creator') }}</option>
                                             <option value="3">{{ __('Member') }}</option>
@@ -98,13 +95,13 @@
 
                                 <div class="mt-3">
                                     <div class=" ">
-                                        <input type="password" class="form-control " placeholder="Password" aria-label="Password" aria-describedby="password-addon" name="password" id="password" required>
+                                        <input type="password" class="form-control " placeholder="Password" aria-label="Password" aria-describedby="password-addon" name="password" id="password" required style="background-color: #0F1535; color: white; border-radius: 15px;">
                                         @error('password')
                                             <p class="text-danger text-xs mt-2 mb-2">{{ $message }}</p>
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="form-check form-check-info text-start">
+                                <div class="form-check form-check-info text-start" style="color: rgb(160, 174, 192)">
                                     <input class="form-check-input" type="checkbox" name="agreement" id="flexCheckDefault" checked="">
                                     <label class="form-check-label" for="flexCheckDefault">
                                         I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
@@ -116,7 +113,7 @@
                                 <div class="text-center">
                                     <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
                                 </div>
-                                <p class="text-sm mt-3 mb-0">Already have an account? <a href="{{ url('login') }}" class="text-dark font-weight-bolder">Sign in</a></p>
+                                <p class="text-sm mt-3 mb-0" style="color: rgb(160, 174, 192)">Already have an account? <a href="{{ url('login') }}" class="text-dark font-weight-bolder">Sign in</a></p>
                             </form>
                         </div>
                     </div>
