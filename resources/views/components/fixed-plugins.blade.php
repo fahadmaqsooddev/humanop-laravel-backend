fd582a45ac63c963cca2de9f978be6ad2cef701a
aamir <m.ahtasham@eye4tech.com>
2024-05-20 11:31:38 +0500

    chat box create design

--- resources/views/components/fixed-plugins.blade.php
+++ resources/views/components/fixed-plugins.blade.php
@@ -3,11 +3,14 @@
         <i class="fa fa-commenting py-2"> </i>
     </a>
     <div class="card shadow-lg blur" style="background-color: #0f1534 !important;">
-        <div class="card-header pb-0 pt-3  bg-transparent ">
-            <div class="float-start">
-                <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-                <p>See our dashboard options.</p>
-            </div>
+        <div class="card-header pb-0 pt-3" style="background-color: #f2661c">
+                        <div class="float-start d-flex">
+                            <img src="{{asset('assets/img/team-3.jpg')}}" alt="Avatar" class="avatar">
+                            <div class="header-info text-white">
+                                <div class="header-title">Need help?</div>
+                                <div class="header-subtitle">We reply immediately</div>
+                            </div>
+                        </div>
             <div class="float-end mt-4">
                 <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
                     <i class="fa fa-close"></i>
@@ -15,10 +18,24 @@
             </div>
             <!-- End Toggle Button -->
         </div>
-        <hr class="horizontal dark my-1">
-        <div class="card-body pt-sm-3 pt-0">
-            <!-- Sidebar Backgrounds -->
-
+        <div class="chatbox">
+            <div class="chatbox-content" id="chatbox-content">
+                <div class="message bot-message">Welcome to our store! Whether you have a specific question or need
+                    assistance, we're here for you. What would you like to know? 😊
+                </div>
+                <div class="message user-message">Shopping guide </div>
+                <div class="message bot-message">Don’t spend hours searching for the right product. We’ll help you find
+                    exactly what you need in no-time! Also, feel free to check our items on sale now. 💥
+                </div>
+                <div class="message user-message">Shopping guide </div>
+                <div class="message bot-message">Don’t spend hours searching for the right product. We’ll help you find
+                    exactly what you need in no-time! Also, feel free to check our items on sale now. 💥
+                </div>
+            </div>
+            <div class="chatbox-input">
+                <input type="text" id="user-input" placeholder="Type your message here...">
+                <button id="send-button">&#9658;</button>
+            </div>
         </div>
     </div>
 </div>
