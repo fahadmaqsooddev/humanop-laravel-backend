f5c64899945b330339a65d6a3db72d4cc43654cb
mahtasham060 <mahtasham060@gmail.com>
2024-06-14 17:24:41 +0500

    editor add in code manage page

--- resources/views/livewire/admin/manage-code/manage-code-form.blade.php
+++ resources/views/livewire/admin/manage-code/manage-code-form.blade.php
@@ -6,8 +6,7 @@
             <label class="form-label text-white">Name</label>
             <div class="input-group">
                 <input style="background-color: #0f1534;" name="name"
-                       class="form-control text-white" type="text" wire:model.defer="select_code.name"
-                       >
+                       class="form-control text-white" type="text" wire:model.defer="select_code.name">
             </div>
         </div>
         <div class="col-12 mt-4">
@@ -36,11 +35,36 @@
         </div>
         <div class="col-12 mt-4">
             <label class="form-label text-white">Text</label>
-            <div class="input-group">
-                                    <textarea rows="10" cols="10" style="background-color: #0f1534;" name="question"
-                                              class="form-control text-white" type="text" wire:model.defer="select_code.text"></textarea>
+            <div class="input-group" wire:ignore>
+                <textarea id="summernote" rows="10" cols="10" style="background-color: #0f1534;" name="text"
+                          class="form-control text-white summernote" wire:model.defer="select_code.text"></textarea>
             </div>
         </div>
     </div>
     <button class="btn btn-sm float-end mt-6 mb-0 text-white" style="background-color: #f2661c">Update code</button>
 </form>
+
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
+<script type="text/javascript">
+    document.addEventListener('livewire:load', function () {
+        $('#summernote').summernote({
+            height: 300,
+            callbacks: {
+                onChange: function(contents, $editable) {
+                @this.set('select_code.text', contents);
+                }
+            }
+        });
+
+        Livewire.on('contentUpdated', function(content) {
+            $('#summernote').summernote('code', content);
+        });
+    });
+
+    document.addEventListener('livewire:update', function () {
+        const content = @this.get('select_code.text');
+        $('#summernote').summernote('code', content);
+    });
+</script>
