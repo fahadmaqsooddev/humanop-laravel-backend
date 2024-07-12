6fe3ee25b318c0f0cb8bb9c0ab8787e9a310ba76
mahtasham060 <mahtasham060@gmail.com>
2024-07-12 15:00:29 +0500

    drop plan table

---
+++ database/migrations/2024_07_12_074447_drop_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('plans');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('plan_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('billing_method')->nullable();
+            $table->string('price')->nullable();
+            $table->string('currency')->nullable();
+
+            $table->timestamps();
+        });
+    }
+};
