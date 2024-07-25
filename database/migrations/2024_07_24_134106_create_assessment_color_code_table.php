14ddc5249160007b8d31770e08855e4feef5982c
mahtasham060 <mahtasham060@gmail.com>
2024-07-25 13:24:39 +0500

    color and code create on assessmentColorCode table

---
+++ database/migrations/2024_07_24_134106_create_assessment_color_code_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessment_color_code', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('assessment_id')->nullable();
+            $table->string('code')->nullable();
+            $table->string('code_color')->nullable();
+
+            $table->foreign('assessment_id')->references('id')->on('assessments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessment_color_code');
+    }
+};
