ed07add08f56e998fdbbe08a550d314f50e50a55
hamza2112 <farooqhamza94@gmail.com>
2024-07-26 18:06:44 +0500

    upload default image

--- app/Http/Controllers/UploadsController.php
+++ app/Http/Controllers/UploadsController.php
@@ -49,7 +49,7 @@ class UploadsController extends Controller
         if (!File::exists($path)){
             // abort 404;
 //            $path = base_path() . '/public/assets/image/image_placeholder.png';
-            $path = storage_path() . '/default_images/original/image_placeholder.png';
+            $path = storage_path() . '/default_images/image_placeholder.png';
             $file = File::get($path);
             $type = File::mimeType($path);
             $response = Response::make($file,200);
