483d07ff9d428414d47c3140314e83d64b91b086
mahtasham060 <mahtasham060@gmail.com>
2024-08-07 18:26:57 +0500

    pdf file changing has been done

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf as PDF;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Assessment;
+use App\Models\AssessmentColorCode;
+
 
 class PDFController extends Controller
 {
@@ -14,6 +16,11 @@ class PDFController extends Controller
 
         $reports = Assessment::getReport($id);
 
+        $alchl_code = Assessment::getAlchlCode($id);
+
+        $style_position = AssessmentColorCode::getStylePosition($id);
+        $feature_position = AssessmentColorCode::getFeaturePosition($id);
+        
         $contxt = stream_context_create([
             'ssl' => [
                 'verify_peer' => FALSE,
@@ -25,7 +32,7 @@ class PDFController extends Controller
         $pdf = PDF::setOptions(['isHTML5ParserEnabled' => true, 'isRemoteEnabled' => true]);
         $pdf->getDomPDF()->setHttpContext($contxt);
 
-        $pdf->loadView('pdf.report_pdf', compact('reports'))->setOptions(['defaultFont' => 'Poppins, sans-serif']);
+        $pdf->loadView('pdf.report_pdf', compact('reports', 'alchl_code','style_position','feature_position'))->setOptions(['defaultFont' => 'Poppins, sans-serif']);
 
         $filename = $reports['user_name']. '_report.pdf';
 
