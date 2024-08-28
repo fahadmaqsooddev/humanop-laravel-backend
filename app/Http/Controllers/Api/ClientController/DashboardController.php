2b43ede01d52b06baf90ef47357d7bf831ed758e
M.Hamza Farooq <55729330+Hamza2112@users.noreply.github.com>
2024-08-28 16:14:04 +0500

    core stats api

--- app/Http/Controllers/Api/ClientController/DashboardController.php
+++ app/Http/Controllers/Api/ClientController/DashboardController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\ClientController;
 
 use App\Helpers\Helpers;
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Code\CodeDetail;
 use App\Models\Admin\DailyTip\DailyTip;
 use App\Models\Admin\Podcast\Podcast;
+use App\Models\Assessment;
 
 class DashboardController extends Controller
 {
@@ -35,6 +37,35 @@ class DashboardController extends Controller
             $podcast = Podcast::getPodcast();
 
             return Helpers::successResponse('Podcast url', $podcast);
+
+        }catch (\Exception $exception){
+
+            return Helpers::serverErrorResponse($exception->getMessage());
+        }
+
+    }
+
+    public function coreStats(){
+
+        try {
+
+            $assessment = Assessment::singleAssessment(Helpers::getUser()->id);
+            $topThreeStyles = $assessment != null ? Assessment::getTopThreeStyles($assessment) : [];
+            $topFeatures = $assessment != null ? Assessment::getFeatures($assessment) : [];
+            $boundary = $assessment != null ? Assessment::getAlchemyPublicName($assessment) : [];
+            $communication = $assessment != null ? Assessment::getEnergy($assessment) : [];
+            $topTwoFeatures = $topFeatures != null ? CodeDetail::getPublicNames($topFeatures['top_two_keys']) : [];
+            $topCommunication = $communication != null ? CodeDetail::getSinglePublicName($communication[0]) : [];
+
+            $data = [
+                'assessment' => $assessment,
+                'topThreeStyles' => $topThreeStyles,
+                'boundry' => $boundary,
+                'topTwoFeatures' => $topTwoFeatures,
+                'topCommunication' => $topCommunication
+            ];
+
+            return Helpers::successResponse('core stats', $data);
 
         }catch (\Exception $exception){
 
--- routes/client_apis/dashboard/dashboard_api.php
+++ routes/client_apis/dashboard/dashboard_api.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('daily_tip', 'DashboardController@dailyTip');
 Route::get('podcast', 'DashboardController@latestPodcast');
+Route::get('core-stats', 'DashboardController@coreStats');
