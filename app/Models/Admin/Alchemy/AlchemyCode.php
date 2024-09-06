25d7e6aff161c02928e0f46c80f1b4af84d296ca
hamza2112 <farooqhamza94@gmail.com>
2024-09-06 11:32:35 +0500

    typo mistake

--- app/Models/Admin/Alchemy/AlchemyCode.php
+++ app/Models/Admin/Alchemy/AlchemyCode.php
@@ -9,7 +9,7 @@ class AlchemyCode extends Model
 {
     use HasFactory;
 
-    protected $appends = ['image_url','video_url'];
+    protected $appends = ['image_url'];
 
     public function __construct(array $attributes = [])
     {
@@ -24,12 +24,6 @@ class AlchemyCode extends Model
     public function getImageUrlAttribute(){
 
         return asset('assets') . '/' . $this->image;
-
-    }
-
-    public function getVideoUrlAttribute(){
-
-        return asset('assets/video') .'/'. $this->video;
 
     }
 
