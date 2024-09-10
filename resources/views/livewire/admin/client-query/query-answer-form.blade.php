34b5ccbc869053ea9fbedad51ee51f1ebb629e49
mahtasham060 <mahtasham060@gmail.com>
2024-09-10 16:50:51 +0500

    core stats hide

--- resources/views/livewire/admin/client-query/query-answer-form.blade.php
+++ resources/views/livewire/admin/client-query/query-answer-form.blade.php
@@ -34,65 +34,65 @@
                             </form>
                         </div>
                     </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12 mb-4 mt-4">
-                        <div class="card" style="height: 500px;" style="border-radius: 3rem !important;">
-                            <div class="card-body p-3">
-                                <div class="d-flex justify-content-between">
-                                    <p class="text-sm mt-3 text-white text-bold" style="color: rgb(160, 174, 192)">
-                                        CORE STATS</p>
-                                    <p class="text-sm mt-3 text-white text-bold" style="color: rgb(160, 174, 192)">
-                                        Interval of Life: (<span class="text-bold text-sm"
-                                                                 style="color: #f2661c">{{$user_age}}</span>)</p>
-                                </div>
-                                <p class="text-sm mt-3" style="color: rgb(160, 174, 192)"> Top 3 Traits:</p>
-                                <div class="d-flex" style="margin-top: -10px">
-                                    @if($topThreeStyles)
-                                        @foreach($topThreeStyles as $index => $style)
-                                            <p class="fw-bold" style="color: #f2661c">
-                                                ({{ $style }}) {{ $index }}@if(!$loop->last),@endif
-                                            </p>
-                                        @endforeach
-                                    @endif
-                                </div>
-                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Motivational Drivers:</p>
-                                <div class="d-flex" style="margin-top: -10px">
-                                    @if($topTwoFeatures)
-                                        @foreach($topTwoFeatures as $index => $feature)
-                                            <p class="fw-bold" style="color: #f2661c">
-                                                ({{ $feature }}) {{ $index }}@if(!$loop->last),@endif
-                                            </p>
-                                        @endforeach
-                                    @endif
-                                </div>
-                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Tolerance Boundaries:</p>
-                                @if($boundary)
-                                    <p class="fw-bold" style="color: #f2661c; margin-top: -10px">
-                                        ({{ $boundary['code_number'] ?? '' }}) {{ $boundary['public_name'] ?? '' }}
-                                    </p>
-                                @endif
-                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Communication Styles:</p>
-                                <div class="d-flex" style="margin-top: -10px">
-                                    @if($topCommunication)
-                                        @foreach($topCommunication as $communication)
-                                            <p class="fw-bold" style="color: #f2661c">
-                                                {{ $communication }}@if(!$loop->last),@endif
-                                            </p>
-                                        @endforeach
-                                    @endif
-                                </div>
-                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Perception of Life:</p>
-                                <p class="fw-bold" style="color: #f2661c; margin-top: -10px">
-                                    {{ $preception == 40 ? "Negative" : ($preception == 41 ? "Neutral" : ($preception == 42 ? "Positive" : '')) }}
-                                </p>
-                                <p class="text-sm" style="color: rgb(160, 174, 192)">Energy Pool:</p>
-                                @if($energyPool)
-                                    <p class="fw-bold" style="color: #f2661c; margin-top: -10px">
-                                        {{ $energyPool }}
-                                    </p>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
+{{--                    <div class="col-lg-12 col-md-12 col-sm-12 mb-4 mt-4">--}}
+{{--                        <div class="card" style="height: 500px;" style="border-radius: 3rem !important;">--}}
+{{--                            <div class="card-body p-3">--}}
+{{--                                <div class="d-flex justify-content-between">--}}
+{{--                                    <p class="text-sm mt-3 text-white text-bold" style="color: rgb(160, 174, 192)">--}}
+{{--                                        CORE STATS</p>--}}
+{{--                                    <p class="text-sm mt-3 text-white text-bold" style="color: rgb(160, 174, 192)">--}}
+{{--                                        Interval of Life: (<span class="text-bold text-sm"--}}
+{{--                                                                 style="color: #f2661c">{{$user_age}}</span>)</p>--}}
+{{--                                </div>--}}
+{{--                                <p class="text-sm mt-3" style="color: rgb(160, 174, 192)"> Top 3 Traits:</p>--}}
+{{--                                <div class="d-flex" style="margin-top: -10px">--}}
+{{--                                    @if($topThreeStyles)--}}
+{{--                                        @foreach($topThreeStyles as $index => $style)--}}
+{{--                                            <p class="fw-bold" style="color: #f2661c">--}}
+{{--                                                ({{ $style }}) {{ $index }}@if(!$loop->last),@endif--}}
+{{--                                            </p>--}}
+{{--                                        @endforeach--}}
+{{--                                    @endif--}}
+{{--                                </div>--}}
+{{--                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Motivational Drivers:</p>--}}
+{{--                                <div class="d-flex" style="margin-top: -10px">--}}
+{{--                                    @if($topTwoFeatures)--}}
+{{--                                        @foreach($topTwoFeatures as $index => $feature)--}}
+{{--                                            <p class="fw-bold" style="color: #f2661c">--}}
+{{--                                                ({{ $feature }}) {{ $index }}@if(!$loop->last),@endif--}}
+{{--                                            </p>--}}
+{{--                                        @endforeach--}}
+{{--                                    @endif--}}
+{{--                                </div>--}}
+{{--                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Tolerance Boundaries:</p>--}}
+{{--                                @if($boundary)--}}
+{{--                                    <p class="fw-bold" style="color: #f2661c; margin-top: -10px">--}}
+{{--                                        ({{ $boundary['code_number'] ?? '' }}) {{ $boundary['public_name'] ?? '' }}--}}
+{{--                                    </p>--}}
+{{--                                @endif--}}
+{{--                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Communication Styles:</p>--}}
+{{--                                <div class="d-flex" style="margin-top: -10px">--}}
+{{--                                    @if($topCommunication)--}}
+{{--                                        @foreach($topCommunication as $communication)--}}
+{{--                                            <p class="fw-bold" style="color: #f2661c">--}}
+{{--                                                {{ $communication }}@if(!$loop->last),@endif--}}
+{{--                                            </p>--}}
+{{--                                        @endforeach--}}
+{{--                                    @endif--}}
+{{--                                </div>--}}
+{{--                                <p class="text-sm" style="color: rgb(160, 174, 192)"> Perception of Life:</p>--}}
+{{--                                <p class="fw-bold" style="color: #f2661c; margin-top: -10px">--}}
+{{--                                    {{ $preception == 40 ? "Negative" : ($preception == 41 ? "Neutral" : ($preception == 42 ? "Positive" : '')) }}--}}
+{{--                                </p>--}}
+{{--                                <p class="text-sm" style="color: rgb(160, 174, 192)">Energy Pool:</p>--}}
+{{--                                @if($energyPool)--}}
+{{--                                    <p class="fw-bold" style="color: #f2661c; margin-top: -10px">--}}
+{{--                                        {{ $energyPool }}--}}
+{{--                                    </p>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
             </div>
         </div>
