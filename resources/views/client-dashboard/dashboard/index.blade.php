89f1c1eaec0d7108c7805bbc10d4eb5b3fce0411
mahtasham060 <mahtasham060@gmail.com>
2024-09-24 18:51:10 +0500

    content lenght decrease

--- resources/views/client-dashboard/dashboard/index.blade.php
+++ resources/views/client-dashboard/dashboard/index.blade.php
@@ -253,8 +253,8 @@
                                     <div class="description-container" style="height: 375px;">
                                         <p class="text-sm mt-3 fs-12px" style="color: rgb(160, 174, 192);">
                                             @if($tip && !empty($tip['text']))
-                                                @if(strlen($tip['text']) > 490)
-                                                    {!! substr($tip['text'], 0, 490)!!}
+                                                @if(strlen($tip['text']) > 260)
+                                                    {!! substr($tip['text'], 0, 265)!!}
                                                     &nbsp;&nbsp;
                                                     <a href="javascript:void(0)" data-bs-toggle="modal"
                                                        data-bs-target="#dailyTipModal" style="color: #f2661c;">read
@@ -395,8 +395,8 @@
                                         NEXT {{ $userPlanName == 'Core' ? '30' : ($userPlanName == 'Premium' ? '7' : '90') }}
                                         DAYS</p>
                                     @if($plan && !empty($plan['text']))
-                                        @if(strlen($plan['text']) > 580)
-                                            {!! substr($plan['text'], 0, 580)!!}
+                                        @if(strlen($plan['text']) > 260)
+                                            {!! substr($plan['text'], 0, 265)!!}
                                             &nbsp;&nbsp;
                                             <a href="javascript:void(0)" data-bs-toggle="modal"
                                                data-bs-target="#actionPlanModal" style="color: #f2661c;">read
