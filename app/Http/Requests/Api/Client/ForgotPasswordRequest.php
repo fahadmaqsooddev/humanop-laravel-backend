98656ab0c40b5f4a8cc67d603d0ac9f66bf80765
hamza2112 <farooqhamza94@gmail.com>
2024-09-25 17:26:03 +0500

    forgot password api done

---
+++ app/Http/Requests/Api/Client/ForgotPasswordRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Api\Client;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ForgotPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|exists:users,email,deleted_at,NULL',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email is required',
+            'email.exists' => 'Email does not match our record'
+        ];
+    }
+}
