cecb111c51a97b734fa48330eec831ab5b944e1c
hamza2112 <farooqhamza94@gmail.com>
2024-10-03 16:35:05 +0500

    update all users status to 1 and dob maximum value to 2006

--- database/seeders/AddAdminAndTheirPermissions.php
+++ database/seeders/AddAdminAndTheirPermissions.php
@@ -20,122 +20,124 @@ class AddAdminAndTheirPermissions extends Seeder
     public function run()
     {
 
-        $oldUsers = [
-            ['email' => 'lisa@humanoptech.com', 'password' => 'lisa@humanoptech','first_name' => 'Lisa', 'last_name' => 'Nelson', 'gender' => Admin::IS_FEMALE, 'role' => Admin::IS_ADMIN],
-            ['email' => 'wei@humanoptech.com', 'password' => 'wei@humanoptech','first_name' => 'Wei', 'last_name' => 'Houng', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'zannah@humanoptech.com', 'password' => 'zannah@humanoptech','first_name' => 'Zannah', 'last_name' => 'Hackett', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'brant@humanop.ai', 'password' => 'brant@humanop','first_name' => 'Brant', 'last_name' => 'Hindman', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'admin@humanoptech.com', 'password' => '12345678','first_name' => 'Admin', 'last_name' => 'Developers', 'gender' => Admin::IS_MALE, 'role' => Admin::IS_ADMIN],
-        ];
-
-        $users = [
-            ['email' => 'lisa@humanop.com', 'password' => 'lisa@humanop','first_name' => 'Lisa', 'last_name' => 'Nelson', 'gender' => Admin::IS_FEMALE, 'role' => Admin::IS_ADMIN],
-            ['email' => 'wei@humanop.com', 'password' => 'wei@humanop','first_name' => 'Wei', 'last_name' => 'Houng', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'zannah@humanop.com', 'password' => 'zannah@humanop','first_name' => 'Zannah', 'last_name' => 'Hackett', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'brant@humanop.com', 'password' => 'Brant@humanop','first_name' => 'Brant', 'last_name' => 'Hindman', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
-            ['email' => 'admin@humanop.com', 'password' => '12345678','first_name' => 'Admin', 'last_name' => 'Developers', 'gender' => Admin::IS_MALE, 'role' => Admin::IS_ADMIN],
-        ];
-
-        $clients = [
-            ['email' => 'apple@humanop.com', 'password' => 'weloveapple@humanop.com', 'first_name' => 'Apple', 'last_name' => 'User', 'gender' => Admin::IS_MALE]
-        ];
-
+//        $oldUsers = [
+//            ['email' => 'lisa@humanoptech.com', 'password' => 'lisa@humanoptech','first_name' => 'Lisa', 'last_name' => 'Nelson', 'gender' => Admin::IS_FEMALE, 'role' => Admin::IS_ADMIN],
+//            ['email' => 'wei@humanoptech.com', 'password' => 'wei@humanoptech','first_name' => 'Wei', 'last_name' => 'Houng', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'zannah@humanoptech.com', 'password' => 'zannah@humanoptech','first_name' => 'Zannah', 'last_name' => 'Hackett', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'brant@humanop.ai', 'password' => 'brant@humanop','first_name' => 'Brant', 'last_name' => 'Hindman', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'admin@humanoptech.com', 'password' => '12345678','first_name' => 'Admin', 'last_name' => 'Developers', 'gender' => Admin::IS_MALE, 'role' => Admin::IS_ADMIN],
+//        ];
+//
+//        $users = [
+//            ['email' => 'lisa@humanop.com', 'password' => 'lisa@humanop','first_name' => 'Lisa', 'last_name' => 'Nelson', 'gender' => Admin::IS_FEMALE, 'role' => Admin::IS_ADMIN],
+//            ['email' => 'wei@humanop.com', 'password' => 'wei@humanop','first_name' => 'Wei', 'last_name' => 'Houng', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'zannah@humanop.com', 'password' => 'zannah@humanop','first_name' => 'Zannah', 'last_name' => 'Hackett', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'brant@humanop.com', 'password' => 'Brant@humanop','first_name' => 'Brant', 'last_name' => 'Hindman', 'gender' => Admin::IS_MALE, 'role' => Admin::SUB_ADMIN],
+//            ['email' => 'admin@humanop.com', 'password' => '12345678','first_name' => 'Admin', 'last_name' => 'Developers', 'gender' => Admin::IS_MALE, 'role' => Admin::IS_ADMIN],
+//        ];
+//
+//        $clients = [
+//            ['email' => 'apple@humanop.com', 'password' => 'weloveapple@humanop.com', 'first_name' => 'Apple', 'last_name' => 'User', 'gender' => Admin::IS_MALE]
+//        ];
+//
         DB::beginTransaction();
 
         try {
+//
+//            foreach ($users as $user){
+//
+//                $findUsers = User::where('email', $user['email'])->get();
+//
+//                foreach ($findUsers as $findUser){
+//
+//                    DB::table('model_has_roles')->where('model_id', $findUser->id)->delete();
+//
+//                    DB::table('model_has_permissions')->where('model_id', $findUser->id)->delete();
+//
+//                    $findUser->delete();
+//                }
+//
+//                $newUser = User::create([
+//                    'first_name' => $user['first_name'],
+//                    'last_name' => $user['last_name'],
+//                    'password' => $user['password'],
+//                    'email' => $user['email'],
+//                    'gender' => $user['gender'],
+//                    'is_admin' => $user['role'],
+//                    'status' => 1,
+//                ]);
+//
+//                if ($user['role'] === 3){
+//
+//                    $role_id = Role::where('name', 'sub admin')->first()->id ?? null;
+//
+//                }else if ($user['role'] === 1){
+//
+//                    $role_id = Role::where('name', 'super admin')->first()->id ?? null;
+//                }
+//
+//                DB::table('model_has_roles')->insert([
+//                    ['role_id' => $role_id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
+//                ]);
+//
+//                $permissions = Permission::all();
+//
+//                foreach ($permissions as $permission){
+//
+//                    if ($user['role'] === 3){ // sub admin
+//
+//                        if ($permission->name != 'approveQueries'){
+//
+//                            DB::table('model_has_permissions')->insert([
+//                                ['permission_id' => $permission->id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
+//                            ]);
+//
+//                        }
+//
+//                    }elseif ($user['role'] === 1){ // super admin
+//
+//                        DB::table('model_has_permissions')->insert([
+//                            ['permission_id' => $permission->id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
+//                        ]);
+//
+//                    }
+//
+//                }
+//
+//            }
+//
+//
+//            foreach ($oldUsers as $oldUser){
+//
+//                $findUsers = User::where('email', $oldUser['email'])->get();
+//
+//                foreach ($findUsers as $findUser){
+//
+//                    DB::table('model_has_roles')->where('model_id', $findUser->id)->delete();
+//
+//                    DB::table('model_has_permissions')->where('model_id', $findUser->id)->delete();
+//
+//                    $findUser->delete();
+//                }
+//
+//            }
+//
+//            foreach ($clients as $client) {
+//
+//                User::where('email', $client['email'])->delete();
+//
+//                User::create([
+//                    'first_name' => $client['first_name'],
+//                    'last_name' => $client['last_name'],
+//                    'password' => $client['password'],
+//                    'email' => $client['email'],
+//                    'gender' => $client['gender'],
+//                    'is_admin' => Admin::IS_CUSTOMER,
+//                    'status' => 1,
+//                ]);
+//            }
 
-            foreach ($users as $user){
-
-                $findUsers = User::where('email', $user['email'])->get();
-
-                foreach ($findUsers as $findUser){
-
-                    DB::table('model_has_roles')->where('model_id', $findUser->id)->delete();
-
-                    DB::table('model_has_permissions')->where('model_id', $findUser->id)->delete();
-
-                    $findUser->delete();
-                }
-
-                $newUser = User::create([
-                    'first_name' => $user['first_name'],
-                    'last_name' => $user['last_name'],
-                    'password' => $user['password'],
-                    'email' => $user['email'],
-                    'gender' => $user['gender'],
-                    'is_admin' => $user['role'],
-                    'status' => 1,
-                ]);
-
-                if ($user['role'] === 3){
-
-                    $role_id = Role::where('name', 'sub admin')->first()->id ?? null;
-
-                }else if ($user['role'] === 1){
-
-                    $role_id = Role::where('name', 'super admin')->first()->id ?? null;
-                }
-
-                DB::table('model_has_roles')->insert([
-                    ['role_id' => $role_id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
-                ]);
-
-                $permissions = Permission::all();
-
-                foreach ($permissions as $permission){
-
-                    if ($user['role'] === 3){ // sub admin
-
-                        if ($permission->name != 'approveQueries'){
-
-                            DB::table('model_has_permissions')->insert([
-                                ['permission_id' => $permission->id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
-                            ]);
-
-                        }
-
-                    }elseif ($user['role'] === 1){ // super admin
-
-                        DB::table('model_has_permissions')->insert([
-                            ['permission_id' => $permission->id ?? null, 'model_type' => 'App\Models\User','model_id' => $newUser->id],
-                        ]);
-
-                    }
-
-                }
-
-            }
-
-
-            foreach ($oldUsers as $oldUser){
-
-                $findUsers = User::where('email', $oldUser['email'])->get();
-
-                foreach ($findUsers as $findUser){
-
-                    DB::table('model_has_roles')->where('model_id', $findUser->id)->delete();
-
-                    DB::table('model_has_permissions')->where('model_id', $findUser->id)->delete();
-
-                    $findUser->delete();
-                }
-
-            }
-
-            foreach ($clients as $client) {
-
-                User::where('email', $client['email'])->delete();
-
-                User::create([
-                    'first_name' => $client['first_name'],
-                    'last_name' => $client['last_name'],
-                    'password' => $client['password'],
-                    'email' => $client['email'],
-                    'gender' => $client['gender'],
-                    'is_admin' => Admin::IS_CUSTOMER,
-                    'status' => 1,
-                ]);
-            }
+            User::query()->update(['status' => 1]);
 
             DB::commit();
 
