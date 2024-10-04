b9137e239ae79e72b3d6958335587d0a3ee1caab
hamza2112 <farooqhamza94@gmail.com>
2024-10-04 19:54:17 +0500

    typo

--- app/Http/Requests/Api/Client/LoginRequest.php
+++ app/Http/Requests/Api/Client/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api\Client;
 
+use App\Enums\Admin\Admin;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
@@ -24,7 +25,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|email|exists:users,email,deleted_at,NULL',
+            'email' => 'required|email|exists:users,email,deleted_at,NULL,is_admin,' . Admin::IS_CUSTOMER,
             'password' => 'required'
         ];
     }
