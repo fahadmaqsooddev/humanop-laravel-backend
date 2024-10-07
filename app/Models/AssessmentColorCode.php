5be51ca53625dbe4949361f4efdcd4f31fac2cd3
mahtasham060 <mahtasham060@gmail.com>
2024-10-07 13:04:46 +0500

    grid pdf has been updated

--- app/Models/AssessmentColorCode.php
+++ app/Models/AssessmentColorCode.php
@@ -44,6 +44,18 @@ class AssessmentColorCode extends Model
         return $code_color;
     }
 
+    public static function getAssessmentCodeAndColor($assessmentId = null)
+    {
+        $assessmentCodeColors = self::where('assessment_id', $assessmentId)->get();
+
+        $code_color = [];
+        foreach ($assessmentCodeColors as $assessment) {
+            $code_color[$assessment['code']] = $assessment['code_color'];
+        }
+
+        return $code_color;
+    }
+
     public static function getHighlightCodeColor($assessmentId = null)
     {
         $assessmentCodeColors = self::where('assessment_id', $assessmentId)->whereIn('code_color', ['green', 'border-green'])->get();
