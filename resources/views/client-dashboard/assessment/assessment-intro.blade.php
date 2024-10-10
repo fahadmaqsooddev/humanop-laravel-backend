4134c09bc5e65831ce21e40514857ba9f9fb0d52
mahtasham060 <mahtasham060@gmail.com>
2024-10-10 09:43:52 -0700

    intro assessment responsed

--- resources/views/client-dashboard/assessment/assessment-intro.blade.php
+++ resources/views/client-dashboard/assessment/assessment-intro.blade.php
@@ -40,22 +40,22 @@
         <div class="mt-5">
             <div class="tab-content tab-space mt-5">
                 <div class="tab-pane active" id="monthly">
-                    <div class="row w-80" style="margin-left: 20%">
-                        <div class="col-7">
+                    <div class="row w-100 introAssessmentMargin">
+                        <div class="col-lg-7 col-md-7 col-12">
                             <h3 class="text-bold text-white">Welcome to the HumanOp Assessment</h3>
                             <h4 class="text-white text-bold">"Creating Optimal Relationships in Business, Home &
                                 Life"</h4>
-                            <p class="text-white">Our evolutionary human assessment technology is designed to help you
+                            <p class="text-white" style="display: flex; text-align: justify">Our evolutionary human assessment technology is designed to help you
                                 gain deeper insights into yourself. Its analysis will help you understand your natural
                                 abilities, behaviors, strengths, preferences, perceptions and the unique way you connect
                                 with the world around you. Taking the HumanOp assessment is a great opportunity for you
                                 will learn how to use your natural talents in such a way that is sustainable, and keeps
                                 you energized and optimized. The HumanOp technology stands apart by offering actionable,
                                 physics-based insights that guide you to your highest performance potential.</p>
-                            <p class="text-white">Before you take the HumanOp Assessment make sure that you are NOT
+                            <p class="text-white" style="display: flex; text-align: justify">Before you take the HumanOp Assessment make sure that you are NOT
                                 multi-tasking and ideally in a quiet environment where you can focus on being present to
                                 the following questions.</p>
-                            <p class="text-white">The questions are simple and do not require extensive thought. It
+                            <p class="text-white" style="display: flex; text-align: justify">The questions are simple and do not require extensive thought. It
                                 should take you no longer than 15-minutes to complete.</p>
                             @if(\App\Helpers\Helpers::getWebUser()['practitioner_id'] != null)
                                 <a href="{{\App\Helpers\Practitioner\PractitionerHelpers::makePractitionerUrl('play')}}" style="padding: 10px 16px 10px 16px; border-radius: 7px"
@@ -67,7 +67,7 @@
                                 </a>
                             @endif
                         </div>
-                        <div class="col-5" style="margin-top: 40%">
+                        <div class="col-lg-5 col-md-5 col-12 introAssessmentLogo" >
                             <img  style="width:90%;height: 90%" src="{{asset('assets/img/icons/assessment_intro_icon.png')}}">
                         </div>
                     </div>
