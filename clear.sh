4a27b715b888328c5dc888e951df278cee617d1a
hamza2112 <farooqhamza94@gmail.com>
2024-10-10 17:34:52 +0500

    shell script done

---
+++ clear.sh
@@ -0,0 +1,15 @@
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan clear-compiled
+php artisan event:clear
+php artisan optimize:clear
+php artisan optimize
+php artisan view:cache
+php artisan event:cache
+php artisan config:cache
+composer -n dump-autoload -o
+php artisan inspire
+php artisan migrate
+composer -n install
+php artisan route:clear
