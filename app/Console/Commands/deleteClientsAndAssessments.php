cb341c5e02d16e3a662994626bbf6003eb104ce7
mahtasham060 <mahtasham060@gmail.com>
2024-10-11 04:32:48 -0700

    intro assessment  issue has been resolved

---
+++ app/Console/Commands/deleteClientsAndAssessments.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class deleteClientsAndAssessments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:clientAndAssessments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will delete clients and Assessments';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/ClientController/QuestionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\ClientController;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use App\Models\Assessment;
+use App\Helpers\Helpers;
+
+class QuestionController extends Controller
+{
+
+    protected $question = null;
+
+    public function __construct(Question $question)
+    {
+        $this->question = $question;
+    }
+
+    public function testPlay()
+    {
+        try {
+
+            $user = Helpers::getWebUser();
+
+
+//            $assessmentCheck = Helpers::checkAssessment($user['id']);
+
+//            if($assessmentCheck == true)
+//            {
+//                return redirect()->route('stripe_checkout');
+//            }else
+//            {
+
+            $questions = Question::getQuestion();
+
+            return view('client-dashboard.question.assessment', compact('questions'));
+//            }
+
+        } catch (\Exception $exception) {
+
+            return redirect()->back()->with('error', $exception->getMessage());
+
+        }
+    }
+
+    public static function allAssessment()
+    {
+        try {
+
+            $assessments = Assessment::getAssessment();
+
+            return view('client-dashboard.assessment.index', compact('assessments'));
+
+        } catch (\Exception $exception) {
+
+            return redirect()->back()->with('error', $exception->getMessage());
+
+        }
+    }
+
+    public function introAssessment()
+    {
+        try {
+            $user = Helpers::getWebUser();
+
+            $assessment = Assessment::singleAssessment($user['id']);
+
+            if (empty($assessment) || ($assessment && $assessment['page'] == 0)) {
+                Assessment::createAssessmentData($user['id'], 0);
+            }
+
+            return view('client-dashboard.assessment.assessment-intro');
+
+        } catch (\Exception $exception) {
+
+            return redirect()->back()->with('error', $exception->getMessage());
+        }
+    }
+
+}
