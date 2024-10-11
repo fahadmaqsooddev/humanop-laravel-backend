a2893012e349ebbdc1fcce3389c95a46518cdc5e
Gullzaib786 <95273365+Gullzaib786@users.noreply.github.com>
2024-10-11 16:24:01 +0500

    assessment intro logo align

--- resources/views/client-dashboard/assessment/assessment-intro.blade.php
+++ resources/views/client-dashboard/assessment/assessment-intro.blade.php
@@ -18,18 +18,19 @@
     .bg-pricing {
         height: 100vh;
     }
+
     .container {
-        margin-left: 0px !important;
-        padding-left: 0px !important;
+        display: none !important;
     }
-    .navbar{
-        padding-left: 0px !important;
-    }
+
     .navbar-brand{
         width: 280px;
     }
     .body-background {
         background-color:  #1C365E !important;
+    }
+    .pl-14{
+        padding-left: 14px !important;
     }
     @media (min-width: 390px) and (max-width: 768px) {
         .introAssessmentMargin {
@@ -48,7 +49,7 @@
         }
         .introAssessmentLogo
         {
-            padding-top: 20%;
+            padding-top: 35%;
         }
     }
 </style>
@@ -56,38 +57,40 @@
 
 
 
-    <div class="mt-5 pt-5">
-        <div class="mt-5">
-            <div class="tab-content tab-space mt-5">
+    <div >
+        <div >
+            <div class="tab-content tab-space ">
                 <div class="tab-pane active" id="monthly">
                     <div class="row w-100 introAssessmentMargin">
-                        <div class="col-lg-7 col-md-7 col-12">
-                            <h3 class="text-bold text-white">Welcome to the HumanOp Assessment</h3>
-                            <h4 class="text-white text-bold">"Creating Optimal Relationships in Business, Home &
+                        <div class="col-lg-7 col-md-7 col-12 mt-2">
+                            <img src="{{ Route::is('client_intro_assessment') ? asset('assets/img/new_logo.png') : asset('assets/img/new_logo.png') }}" alt=""
+                                 style="width: auto; height: 100px">
+                            <h3 class="text-bold text-white mt-3 pl-14">Welcome to the HumanOp Assessment</h3>
+                            <h4 class="text-white text-bold pl-14">"Creating Optimal Relationships in Business, Home &
                                 Life"</h4>
-                            <p class="text-white" style="display: flex; text-align: justify">Our evolutionary human assessment technology is designed to help you
+                            <p class="text-white pl-14" style="display: flex; text-align: justify">Our evolutionary human assessment technology is designed to help you
                                 gain deeper insights into yourself. Its analysis will help you understand your natural
                                 abilities, behaviors, strengths, preferences, perceptions and the unique way you connect
                                 with the world around you. Taking the HumanOp assessment is a great opportunity for you
                                 will learn how to use your natural talents in such a way that is sustainable, and keeps
                                 you energized and optimized. The HumanOp technology stands apart by offering actionable,
                                 physics-based insights that guide you to your highest performance potential.</p>
-                            <p class="text-white" style="display: flex; text-align: justify">Before you take the HumanOp Assessment make sure that you are NOT
+                            <p class="text-white pl-14" style="display: flex; text-align: justify">Before you take the HumanOp Assessment make sure that you are NOT
                                 multi-tasking and ideally in a quiet environment where you can focus on being present to
                                 the following questions.</p>
-                            <p class="text-white" style="display: flex; text-align: justify">The questions are simple and do not require extensive thought. It
+                            <p class="text-white pl-14" style="display: flex; text-align: justify">The questions are simple and do not require extensive thought. It
                                 should take you no longer than 15-minutes to complete.</p>
                             @if(\App\Helpers\Helpers::getWebUser()['practitioner_id'] != null)
-                                <a href="{{\App\Helpers\Practitioner\PractitionerHelpers::makePractitionerUrl('play')}}" style="padding: 10px 16px 10px 16px; border-radius: 7px"
+                                <a href="{{\App\Helpers\Practitioner\PractitionerHelpers::makePractitionerUrl('play')}}" style="padding: 10px 16px 10px 16px; border-radius: 7px;margin-left: 14px"
                                    class="rainbow-border-assessment-intro-btn w-25 text-center">Proceed
                                 </a>
                             @else
-                                <a href="{{url('client/play')}}" style="padding: 10px 16px 10px 16px; border-radius: 7px"
+                                <a href="{{url('client/play')}}" style="padding: 10px 16px 10px 16px; border-radius: 7px;margin-left: 14px"
                                    class="rainbow-border-assessment-intro-btn w-25 text-center">Proceed
                                 </a>
                             @endif
                         </div>
-                        <div class="col-lg-5 col-md-5 col-12 introAssessmentLogo" >
+                        <div class="col-lg-5 col-md-5 col-12 introAssessmentLogo pl-14" >
                             <img  style="width:90%;height: 90%" src="{{asset('assets/img/icons/assessment_intro_icon.png')}}">
                         </div>
                     </div>
