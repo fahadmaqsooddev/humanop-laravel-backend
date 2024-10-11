c950d97f6e87b4ad14461f33945c4f322587bded
mahtasham060 <mahtasham060@gmail.com>
2024-10-11 09:08:04 -0700

    color added

--- resources/views/session/login.blade.php
+++ resources/views/session/login.blade.php
@@ -1,5 +1,9 @@
 @extends('user_type.guest', ['parentFolder' => 'session', 'childFolder' => 'none'])
-
+<style>
+    .left-nav-blue-light-color{
+        background: #2C4C7E !important;
+    }
+</style>
 @section('content')
     <main class="main-content mt-0">
         <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
