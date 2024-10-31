90b24127da944783c78283a13724408de994997b
Gullzaib786 <95273365+Gullzaib786@users.noreply.github.com>
2024-10-31 11:51:17 +0500

    updating intention plan in setting

--- app/Http/Livewire/Client/Setting/IntentionPlanForm.php
+++ app/Http/Livewire/Client/Setting/IntentionPlanForm.php
@@ -9,35 +9,29 @@ use App\Helpers\Helpers;
 
 class IntentionPlanForm extends Component
 {
-    public $userId, $selectIntention;
+    public $userId, $selectedIntention = [];
 
     public function mount()
     {
         $user = Helpers::getWebUser();
-        $intentionPlan = IntentionPlan::getIntentionPlan($user['id']);
-        $this->intention = $intentionPlan ?? '';
+        $this->selectedIntention = IntentionPlan::getIntentionPlan($user['id'])->pluck('intention_option_id')->toArray();
         $this->userId = $user['id'];
     }
 
     public function submitForm()
     {
         try {
-
-            IntentionPlan::updateIntentionPlan($this->userId, $this->intention);
-
+            IntentionPlan::updateIntentionPlan($this->userId, $this->selectedIntention);
             session()->flash('success', '90 Days Intention Plan updated successfully.');
-
         } catch (\Exception $exception) {
-
             session()->flash('error', $exception->getMessage());
-
         }
     }
 
     public function render()
     {
         $intentionOptions = IntentionOption::getOptions();
-
         return view('livewire.client.setting.intention-plan-form', ['intentionOptions' => $intentionOptions]);
     }
 }
+
--- resources/views/livewire/client/setting/intention-plan-form.blade.php
+++ resources/views/livewire/client/setting/intention-plan-form.blade.php
@@ -7,22 +7,18 @@
     <form wire:submit.prevent="submitForm" class="mb-4">
         <div class="card-body pt-0">
             <div class="form-group text-color-dark">
-
                 @foreach($intentionOptions as $option)
                     <div class="form-check">
                         <input type="checkbox"
-                               @foreach($intention as $userIntention)
-                               {{$userIntention['intentionOptions'] ? $userIntention['intentionOptions']['id'] == $option['id'] ? 'checked' : '' : ''}}
-                               @endforeach
-                               name="intention" value="{{$option['id']}}"
+                               value="{{ $option['id'] }}"
+                               wire:model="selectedIntention"
                                class="form-check-input">
                         <label for="name" style="color: #0f1535; font-size: 15px">
                             {{ $option['description'] }}
                         </label>
                     </div>
                 @endforeach
-                <button type="submit" class=" btn-sm float-end mt-4 mb-3 rainbow-border-user-nav-btn">Update Intention
-                </button>
+                <button type="submit" class="btn-sm float-end mt-4 mb-3 rainbow-border-user-nav-btn">Update Intention</button>
             </div>
         </div>
     </form>
