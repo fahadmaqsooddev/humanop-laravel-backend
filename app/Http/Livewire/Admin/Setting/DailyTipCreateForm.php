ddba776b36e5ad283705d5560f8fb0bd794f79a3
Gullzaib786 <95273365+Gullzaib786@users.noreply.github.com>
2024-11-20 19:35:03 +0500

    create daily tip with point

--- app/Http/Livewire/Admin/Setting/DailyTipCreateForm.php
+++ app/Http/Livewire/Admin/Setting/DailyTipCreateForm.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 
 class DailyTipCreateForm extends Component
 {
-    public $title,$description,$code,$tip_id,$point,$interval_of_life;
+    public $title,$description,$code,$tip_id,$point,$interval_of_life,$pv,$ep;
     public $subscription_type = 'Freemium';
     public $point_array = ['SA' => 0, 'MA' => 0, 'JO' => 0, 'LU' => 0, 'VEN' => 0, 'MER' => 0, 'SO' => 0,'DE' => 0, 'DOM' => 0, 'FE' => 0, 'GRE' => 0, 'LUN' => 0, 'NAI' => 0, 'NE' => 0, 'POW' => 0, 'SP' => 0, 'TRA' => 0, 'VAN' => 0, 'WIL' => 0,'G' => 502, 'S' => 52, 'C' => 7,'GS' => 403,'SG' => 322,'SC' => 43,'CS' => 34,'EM' => 3, 'INS' => 3, 'INT' => 3, 'MOV' => 3];
     public $interval_of_life_array = ['motivation_life_cycle' => 'Motivation Life Cycle','roadworthy_life_cycle' => 'Roadworthy Life Cycle','power_life_cycle' => 'Power Life Cycle','mid_life_cycle' => 'Mid Life Cycle','awareness_life_cycle' => 'Awareness Life Cycle','forward_life_cycle' => 'Forward Life Cycle','liberated_life_cycle' => 'Liberated Life Cycle','being_life_cycle' => 'Being Life Cycle','review_life_cycle' => 'Review Life Cycle'];
@@ -28,16 +28,12 @@ class DailyTipCreateForm extends Component
         'title.required' => 'Title is required',
         'subscription_type.required' => 'Please Select Subscription Type',
         'description.required' => 'Description is required',
-
     ];
 
     public function selectCode($selectedCode){
-        $this->code = $selectedCode;
-        if(in_array($this->code,['+', '-', 'PV', 'EP'])){
-            $this->point = null;
-        }else{
+            $this->code = $selectedCode;
             $this->point = $this->point_array[$this->code];
-        }
+            $this->emit('codeSelected');
     }
 
        public function updateContent($editorId, $data)
@@ -63,9 +59,14 @@ class DailyTipCreateForm extends Component
 
       public function updateTip(){
         try {
-
+             if(isset($this->ep)){
+                 $this->code = 'ep';
+                 $this->point = $this->ep;
+             }else if (isset($this->pv)){
+                 $this->code = 'pv';
+                 $this->point = $this->pv;
+             }
             $validatedData = $this->validate();
-
             if($this->tip_id){
                 DailyTip::updateIntentionPlan($validatedData,$this->tip_id);
                 $this->emit('closeModal');
