d8ff24d7c0fb42d5fea14f09c81a6c6fa30d16be
mahtasham060 <mahtasham060@gmail.com>
2024-11-25 16:39:14 +0500

    gender is required

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -196,7 +196,7 @@
                                                         Birth</label>
                                                     <select class="form-control" name="gender" id="gender"
                                                             style="background-color: #f3deba; color: black; border-radius: 15px;">
-                                                        <option value="" selected hidden>Gender at Birth
+                                                        <option value="" selected >Gender at Birth
                                                         </option>
                                                         <option value="0" {{old('gender') == 0 ? 'selected' : ''}}>Male
                                                             (XY)
