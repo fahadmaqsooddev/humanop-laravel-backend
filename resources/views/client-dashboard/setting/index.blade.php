d909cc4b261c9b64e4c53f57805dbd2232a85711
mahtasham060 <mahtasham060@gmail.com>
2024-11-26 12:37:23 +0500

    croper issue has been done

--- resources/views/client-dashboard/setting/index.blade.php
+++ resources/views/client-dashboard/setting/index.blade.php
@@ -19,6 +19,18 @@
             font-weight: bold;
         }
 
+        img {
+            display: block;
+            max-width: 100%;
+        }
+
+        .preview {
+            overflow: hidden;
+            width: 160px;
+            height: 160px;
+            margin: 10px;
+            border: 1px solid red;
+        }
     </style>
 @endpush
 
@@ -432,6 +444,38 @@
         </div>
         @include('layouts/footers/auth/footer')
     </div>
+
+    {{--  Profile image cropper model   --}}
+    <div class="modal fade" id="profileImageModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <label class="form-label fs-4 text-white">Update User Profile</label>
+                    <button type="button" class="close modal-close-btn" data-bs-dismiss="modal"
+                            aria-label="Close" id="close-info-modal-button">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <div class="img-container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <!--  default image where we will set the src via jquery-->
+                                <img id="image">
+                            </div>
+                            <div class="col-md-4">
+                                <div class="preview"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-4 float-end">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn" style="background-color: #f2661c; color: white" id="crop">Crop</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @push('js')
@@ -599,5 +643,104 @@
                 }
             })
         }
+    </script>
+
+    <script>
+        var image = document.getElementById('profileImageModal');
+
+        var bs_modal = $('#profileImageModal');
+        var image = document.getElementById('image'); // Correct reference to the image element
+        var cropper, reader, file;
+
+        $(".profileImage").on("change", function (e) {
+            console.log('image model open');
+            var files = e.target.files;
+            var done = function (url) {
+                image.src = url;
+                bs_modal.modal('show');
+            };
+
+            if (files && files.length > 0) {
+                file = files[0];
+
+                if (URL) {
+                    done(URL.createObjectURL(file));
+                } else if (FileReader) {
+                    reader = new FileReader();
+                    reader.onload = function (e) {
+                        done(reader.result);
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+        });
+
+        bs_modal.on('shown.bs.modal', function () {
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                viewMode: 3,
+                preview: '.preview'
+            });
+        }).on('hidden.bs.modal', function () {
+            if (cropper) {
+                cropper.destroy();
+                cropper = null;
+            }
+        });
+
+        function base64ToFile(base64, filename, mimeType) {
+            const byteString = atob(base64.split(',')[1]); // Decode base64 string
+            const arrayBuffer = new ArrayBuffer(byteString.length);
+            const uint8Array = new Uint8Array(arrayBuffer);
+
+            for (let i = 0; i < byteString.length; i++) {
+                uint8Array[i] = byteString.charCodeAt(i);
+            }
+
+            return new File([uint8Array], filename, {type: mimeType});
+        }
+
+        $("#crop").click(function () {
+            if (cropper) {
+                var canvas = cropper.getCroppedCanvas({
+                    width: 160,
+                    height: 160,
+                });
+
+                canvas.toBlob(function (blob) {
+                    const reader = new FileReader();
+                    reader.readAsDataURL(blob);
+                    reader.onloadend = function () {
+                        const base64data = reader.result;
+
+                        // Convert base64data to a File object
+                        const file = base64ToFile(base64data, 'cropped-image.png', 'image/png');
+
+                        // Example of appending the file to a FormData object
+                        const formData = new FormData();
+                        formData.append('image', file);
+
+                        $.ajax({
+                            type: "POST",
+                            url: '{{ route("user_profile_image") }}',
+                            headers: {
+                                'X-CSRF-TOKEN': "{{csrf_token()}}"
+                            },
+                            data: formData,
+                            processData: false, // Prevent jQuery from processing the data
+                            contentType: false, // Prevent jQuery from setting the content type
+                            success: function (data) {
+                                $('.user_profile_image').attr('src', data.url.url);
+                                bs_modal.modal('hide');
+                            },
+                            error: function () {
+                                console.error("Image upload failed");
+                            }
+                        });
+                    };
+                });
+            }
+        });
+
     </script>
 @endpush
