09dc085d0028720c87654a231934afa550cb5f5c
mahtasham060 <mahtasham060@gmail.com>
2024-12-24 13:26:33 +0500

    tech remove

--- resources/views/session/app-client-email-verified.blade.php
+++ resources/views/session/app-client-email-verified.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-    <title>HumanOp Tech</title>
+    <title>HumanOp</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
           crossorigin="anonymous">
