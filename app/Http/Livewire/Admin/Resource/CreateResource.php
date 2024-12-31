86f06230233da27a15b105bfd99c9abbe867e205
mahtasham060 <mahtasham060@gmail.com>
2024-12-31 20:01:46 +0500

    dd add

--- app/Http/Livewire/Admin/Resource/CreateResource.php
+++ app/Http/Livewire/Admin/Resource/CreateResource.php
@@ -236,6 +236,9 @@ class CreateResource extends Component
 
         if (!empty($this->resource) && in_array($this->resource->extension(), ['mp4']))
         {
+
+            dd($updateResource['video_url']['path']);
+            
             $this->sendRequestFromGuzzle('DELETE', 'https://api.gumlet.com/v1/video/assets/'. $getResource['source_id']);
 
             $responseData = $this->sendRequestFromGuzzle('post', 'https://api.gumlet.com/v1/video/assets',
