69b9e36de8eba633363c96346f3d9cd2d6597a7c
mahtasham060 <mahtasham060@gmail.com>
2025-01-09 13:47:39 +0500

    dd add

--- app/Http/Controllers/Api/ClientController/UserController.php
+++ app/Http/Controllers/Api/ClientController/UserController.php
@@ -90,7 +90,6 @@ class UserController extends Controller
 
             User::updateUser(['app_intro_check' => 1], Helpers::getUser()->id);
 
-
             return Helpers::successResponse('Intro Completed Successfully');
 
         } catch (\Exception $exception) {
