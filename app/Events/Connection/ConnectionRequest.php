1405fe2b97e7d9443add50b189cb1cfd5c262d98
alliy1234 <ab7464895@gmail.com>
2025-01-10 18:44:50 +0500

    connection request event create

---
+++ app/Events/Connection/ConnectionRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events\Connection;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ConnectionRequest implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $friendId;
+    public $heading;
+    public $message;
+    
+    public function __construct($friendId=null,$heading=null,$message=null)
+    {
+        $this->friendId = $friendId;
+        $this->heading = $heading;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+   
+    public function broadcastOn()
+    {
+        return new Channel('push-notification.' . $this->friendId);
+    }
+    public function broadcastAs(){
+    
+        return 'connection.request';
+    }
+}
