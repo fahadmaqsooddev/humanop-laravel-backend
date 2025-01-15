dac1441e989e77049909130b5b14b838e5ca6580
mahtasham060 <mahtasham060@gmail.com>
2025-01-15 16:29:12 +0500

    dd add

--- app/Models/Admin/DailyTip/DailyTip.php
+++ app/Models/Admin/DailyTip/DailyTip.php
@@ -115,6 +115,8 @@ class DailyTip extends Model
 
     public static function getTodayTip()
     {
+        dd(1);
+        
         $user = Helpers::getWebUser() ?? Helpers::getUser();
 
         $assessment = Assessment::getLatestAssessment($user['id']);
