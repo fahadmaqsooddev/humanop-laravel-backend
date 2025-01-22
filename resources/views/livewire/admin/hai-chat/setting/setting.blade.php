3f434f22f3ddd10272322e0aadaee779fa389802
Muhammad Hamza <hamza.waheed00777@gmail.com>
2025-01-22 20:21:59 +0500

    Update setting.blade.php

--- resources/views/livewire/admin/hai-chat/setting/setting.blade.php
+++ resources/views/livewire/admin/hai-chat/setting/setting.blade.php
@@ -88,6 +88,7 @@
                                     wire:model.defer="model_type">
                                 <option value="1">gpt-4o-mini</option>
                                 <option value="2">gpt-4o</option>
+                                <option value="4">gpt-4o-finetunned</option>
                                 <option value="3">Claude 3.5 Sonnet</option>
                             </select>
                         </div>
