0e9d22ea886b91d0e62b49bf3cd58722cbb8f05a
mahtasham060 <mahtasham060@gmail.com>
2025-01-27 18:17:51 +0500

    migration commit

--- database/migrations/2025_01_22_154608_change_column_name_to_chatbot_id_in_chatbots_table.php
+++ database/migrations/2025_01_22_154608_change_column_name_to_chatbot_id_in_chatbots_table.php
@@ -19,13 +19,13 @@ return new class extends Migration
 //
 //        });
 //
-        Schema::table('chatbot_keywords', function (Blueprint $table) {
+//        Schema::table('chatbot_keywords', function (Blueprint $table) {
 //
-            $table->unsignedBigInteger('chatbot_id')->nullable();
+//            $table->unsignedBigInteger('chatbot_id')->nullable();
 //
 //            $table->foreign('chatbot_id')->references('id')->on('chatbots');
 //
-        });
+//        });
     }
 
     /**
@@ -36,19 +36,19 @@ return new class extends Migration
     public function down()
     {
 //        Schema::table('chatbot_keywords', function (Blueprint $table) {
-//
+
 //            $table->unsignedBigInteger('chat_bot_id')->nullable();
-//
-////            $table->foreign('chat_bot_id')->references('id')->on('chat_bots');
-//
+
+//            $table->foreign('chat_bot_id')->references('id')->on('chat_bots');
+
 //        });
 //
-        Schema::table('chatbot_keywords', function (Blueprint $table) {
+//        Schema::table('chatbot_keywords', function (Blueprint $table) {
 //
-            $table->dropColumn(['chatbot_id']);
+//            $table->dropColumn(['chatbot_id']);
 //
 ////            $table->dropConstrainedForeignId('chatbot_id');
 //
-        });
+//        });
     }
 };
