71158029c98bbd220af8c75418a16d7337cccd45
alliy1234 <ab7464895@gmail.com>
2025-01-27 16:02:58 +0500

    cms module changed

--- resources/views/admin-dashboards/web-pages/edit.blade.php
+++ resources/views/admin-dashboards/web-pages/edit.blade.php
@@ -5,8 +5,8 @@
         <div class="col-12">
             <div class="card">
                 <!-- Card header -->
-                <div class="card-header table-header-text">
-                    <h5 class="mb-0">Edit Web Page</h5>
+                <div class="card-header ">
+                    <h5 class="mb-0 text-white">Edit Web Page</h5>
                 </div>
                 <div class="card-body pt-0">
                     @livewire('admin.web-page.web-page-form', ['page' => $web_page])
