fc554241f72459908731a4c465c4d6b4374d2937
alliy1234 <ab7464895@gmail.com>
2025-02-10 14:28:33 +0500

    useless model function delete

--- app/Models/Client/Follow/Follow.php
+++ app/Models/Client/Follow/Follow.php
@@ -77,105 +77,105 @@ class Follow extends Model
     }
 
     // relations
-    public static function addFollow($follow_id = null){
+    // public static function addFollow($follow_id = null){
 
-        $data['follow_id'] = $follow_id;
+    //     $data['follow_id'] = $follow_id;
 
-        $data['user_id'] = Helpers::getWebUser()->id;
+    //     $data['user_id'] = Helpers::getWebUser()->id;
 
-        $follow = self::where('follow_id', $follow_id)->where('user_id', $data['user_id'])->first();
+    //     $follow = self::where('follow_id', $follow_id)->where('user_id', $data['user_id'])->first();
 
-        $followUser = User::getSingleUser($follow_id);
+    //     $followUser = User::getSingleUser($follow_id);
 
-        if ($follow) {
+    //     if ($follow) {
 
-            $follow->delete();
+    //         $follow->delete();
 
-            $msg='Unfollow Request is send';
+    //         $msg='Unfollow Request is send';
 
-            event(new UnFollowRequest($follow_id,'Un-Follow Request',$msg));
+    //         event(new UnFollowRequest($follow_id,'Un-Follow Request',$msg));
 
-            Notification::createNotification('un follow request', $msg, null, $follow_id, 1, Admin::UN_FOLLOW_REQUEST_NOTIFICATION);
+    //         Notification::createNotification('un follow request', $msg, null, $follow_id, 1, Admin::UN_FOLLOW_REQUEST_NOTIFICATION);
 
-            $message = "You unfollowed " . $followUser['first_name'] . " " . $followUser['last_name'] . ".";
+    //         $message = "You unfollowed " . $followUser['first_name'] . " " . $followUser['last_name'] . ".";
 
-            toastr()->success($message);
+    //         toastr()->success($message);
 
-        } else {
+    //     } else {
 
-            self::create($data);
+    //         self::create($data);
 
-            $msg='Follow Request is send';
+    //         $msg='Follow Request is send';
 
-            event(new FollowRequest($follow_id,'Follow Request',$msg));
+    //         event(new FollowRequest($follow_id,'Follow Request',$msg));
 
-            Notification::createNotification('follow request', $msg, null, $follow_id, 1, Admin::FOLLOW_REQUEST_NOTIFICATION);
+    //         Notification::createNotification('follow request', $msg, null, $follow_id, 1, Admin::FOLLOW_REQUEST_NOTIFICATION);
 
-            $message = "You followed " . $followUser['first_name'] . " " . $followUser['last_name'] . ".";
+    //         $message = "You followed " . $followUser['first_name'] . " " . $followUser['last_name'] . ".";
 
-            toastr()->success($message);
-        }
+    //         toastr()->success($message);
+    //     }
 
 
-    }
+    // }
 
-    public static function following($search_name = null){
+    // public static function following($search_name = null){
 
-        $following = self::query();
+    //     $following = self::query();
 
-        if ($search_name){
+    //     if ($search_name){
 
-            $following = $following->whereHas('follower', function ($q) use ($search_name){
+    //         $following = $following->whereHas('follower', function ($q) use ($search_name){
 
-                $q->where('first_name', 'LIKE', "%$search_name%")
+    //             $q->where('first_name', 'LIKE', "%$search_name%")
 
-                    ->orWhere('last_name', 'LIKE', "%$search_name%")
+    //                 ->orWhere('last_name', 'LIKE', "%$search_name%")
 
-                    ->orWhereRaw("concat(first_name, ' ', last_name) like '%$search_name%' ");
+    //                 ->orWhereRaw("concat(first_name, ' ', last_name) like '%$search_name%' ");
 
-            });
+    //         });
 
-        }
+    //     }
 
-        $following = $following->has('follower')->where('user_id', (Helpers::getWebUser()->id ?? Helpers::getUser()->id))
+    //     $following = $following->has('follower')->where('user_id', (Helpers::getWebUser()->id ?? Helpers::getUser()->id))
 
-            ->with('follower:id,first_name,last_name')
+    //         ->with('follower:id,first_name,last_name')
 
-            ->get();
+    //         ->get();
 
-        return $following;
-    }
+    //     return $following;
+    // }
 
-    public static function followerExists($follow_id = null){
+    // public static function followerExists($follow_id = null){
 
-        return self::where('user_id', Helpers::getWebUser()->id)->where('follow_id', $follow_id)->exists();
-    }
+    //     return self::where('user_id', Helpers::getWebUser()->id)->where('follow_id', $follow_id)->exists();
+    // }
 
-    public static function followers($search_name = null){
+    // public static function followers($search_name = null){
 
-        $followers = self::query();
+    //     $followers = self::query();
 
-        if ($search_name){
+    //     if ($search_name){
 
-            $followers = $followers->whereHas('user', function ($q) use ($search_name){
+    //         $followers = $followers->whereHas('user', function ($q) use ($search_name){
 
-                $q->where('first_name', 'LIKE', "%$search_name%")
+    //             $q->where('first_name', 'LIKE', "%$search_name%")
 
-                    ->orWhere('last_name', 'LIKE', "%$search_name%")
+    //                 ->orWhere('last_name', 'LIKE', "%$search_name%")
 
-                    ->orWhereRaw("concat(first_name, ' ', last_name) like '%$search_name%' ");
+    //                 ->orWhereRaw("concat(first_name, ' ', last_name) like '%$search_name%' ");
 
-            });
-        }
+    //         });
+    //     }
 
-        $followers = $followers->has('user')->where('follow_id', (Helpers::getWebUser()->id ?? Helpers::getUser()->id))
+    //     $followers = $followers->has('user')->where('follow_id', (Helpers::getWebUser()->id ?? Helpers::getUser()->id))
 
-            ->with('user:id,first_name,last_name,image_id')
+    //         ->with('user:id,first_name,last_name,image_id')
 
-            ->get();
+    //         ->get();
 
-        return $followers;
-    }
+    //     return $followers;
+    // }
 
     public static function followUnFollowForApi($request = null){
 
