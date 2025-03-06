7aa84ddd862beec4d8b975e1328b9445ca640977
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-03-06 12:39:36 +0500

    Revert "candidate api updated"
    This reverts commit 7934a8d13d5dd28402fd8da3c977f1e08b1f0e2c.

--- app/Models/B2B/B2BBusinessCandidates.php
+++ app/Models/B2B/B2BBusinessCandidates.php
@@ -61,18 +61,10 @@ class B2BBusinessCandidates extends Model
     {
         $businessId = Helpers::getUser()['id'];
 
-        // return self::where('business_id', $businessId)->whereHas('assessments')
-        // ->with(['users', 'assessments'=>function($query){
-        //     $query->where('page',0);
-        // }])->first();
-        return self::where('business_id', $businessId)
-    ->whereHas('assessments')
-    ->with(['users', 'assessments' => function ($query) {
-        $query->where('page', 0);
-    }])
-    ->inRandomOrder() // Randomly orders the results
-    ->first(); // Picks the first random candidate
-
+        return self::where('business_id', $businessId)->whereHas('assessments')
+        ->with(['users', 'assessments'=>function($query){
+            $query->where('page',0);
+        }])->first();
 
     }
 
