cd3155fdcd0158c2c54945dba909397a01f0c66b
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-03-14 15:33:12 +0500

    $this->id return

--- app/Models/User.php
+++ app/Models/User.php
@@ -155,6 +155,7 @@ class User extends Authenticatable implements JWTSubject
     public function getCheckCompanyAttribute()
     {
 
+        return $this->id;
         $dataShareWithBusiness = B2BBusinessCandidates::checkCandidateCompany($this->id);
 
         return $dataShareWithBusiness;
