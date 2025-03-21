f6943d6271a88a0c6053f099e30b7107076d3142
hamza2112 <farooqhamza94@gmail.com>
2025-03-21 09:51:58 +0500

    credentials shifted to config file

---
+++ config/aws.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    's3_bucket' => [
+        'key' => 'AKIASBLHPXE7KK57YW4M',
+        'secret' => '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',
+        'region' => 'us-east-1',
+        'bucket_name' => 'laravel-s3-management',
+
+    ],
+
+];
