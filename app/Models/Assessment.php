3b42ace3b323192739da101b41b89f35cc714458
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-03-26 19:08:47 +0500

    grid hightlight

--- app/Models/Assessment.php
+++ app/Models/Assessment.php
@@ -817,7 +817,7 @@ class Assessment extends Model
                     if (($assessment['gre'] > 2 && ($assessment['jo'] > 6 || $assessment['mer'] > 4 )) || ($assessment['gre'] > 2 && $assessment['ven'] > 4 && $assessment['so'] > 4) || ($assessment['gre'] > 2 && $assessment['ma'] > 4 && $assessment['lu'] > 4)) {
                         $filtered_keys[$key] = $value;
                     }
-                    elseif (($assessment['gre'] > 2 && $assessment['jo'] < 6 && $assessment['mer'] < 5) && ($assessment['gre'] > 2 && $assessment['ma'] < 5 && $assessment['lu'] < 5) && ($assessment['gre'] > 2 && $assessment['ven'] < 5 && $assessment['so'] < 5)) {
+                    elseif (($assessment['gre'] > 2 && $assessment['jo'] < 7 && $assessment['mer'] < 5) && ($assessment['gre'] > 2 && ($assessment['ma'] < 5 || $assessment['lu'] < 5)) && ($assessment['gre'] > 2 && ($assessment['ven'] < 5 || $assessment['so'] < 5))) {
                         $filtered_keys_red[$key] = $value;
                     }
                     break;
