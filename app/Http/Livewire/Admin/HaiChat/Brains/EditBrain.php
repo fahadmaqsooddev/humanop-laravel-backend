18147d8488f0f03a265b5d89ba49a0105e1d6660
hamza2112 <farooqhamza94@gmail.com>
2025-04-04 20:25:33 +0500

    typo

--- app/Http/Livewire/Admin/HaiChat/Brains/EditBrain.php
+++ app/Http/Livewire/Admin/HaiChat/Brains/EditBrain.php
@@ -76,7 +76,7 @@ class EditBrain extends Component
 
                 DB::commit();
 
-                session()->flash('success','Brain created successfully');
+                session()->flash('success','Brain updated');
 
                 return redirect()->route('admin_hai_chat');
 
