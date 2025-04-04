d477947ee24aa6783464e5b00428fba7f2f927e9
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-04-04 13:01:52 +0500

    b2b link changed

--- config/client_url.php
+++ config/client_url.php
@@ -9,12 +9,12 @@ if (env('APP_ENV') == 'local') {
 } elseif (env('APP_ENV') == 'development') {
     return [
         'client_dashboard_url' => 'https://human-op-dev.vercel.app',
-        'b2b_dashboard_url' => 'https://b2b-fc5n.vercel.app',
+        'b2b_dashboard_url' => 'https://human-op-b2b.vercel.app',
     ];
 } elseif (env('APP_ENV') == 'staging') {
     return [
         'client_dashboard_url' => 'https://human-op-staging.vercel.app',
-        'b2b_dashboard_url' => 'https://b2b-fc5n.vercel.app',
+        'b2b_dashboard_url' => 'https://human-op-b2b.vercel.app',
     ];
 } elseif (env('APP_ENV') == 'production') {
     return [
