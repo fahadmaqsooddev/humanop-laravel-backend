3f2e53ba0e11b9196a466ac666434e98671d95af
Muhammad Ali Butt <ab7464895@gmail.com>
2025-04-10 15:54:34 +0500

    add google id and apple id column in users scope

--- app/Models/User.php
+++ app/Models/User.php
@@ -95,7 +95,11 @@ class User extends Authenticatable implements JWTSubject
 
     public function scopeSelection($query)
     {
-        return $query->select(['id', 'first_name', 'last_name', 'gender', 'email', 'phone', 'is_admin', 'is_feedback', 'image_id', 'date_of_birth', 'hai_chat', 'referral_code', 'timezone', 'two_way_auth', 'intro_check', 'app_intro_check', 'step', 'register_from_app', 'email_verified_at', 'company_name']);
+        return $query->select(['id', 'first_name', 'last_name',
+            'gender', 'email', 'phone', 'is_admin', 'is_feedback',
+            'image_id', 'date_of_birth', 'hai_chat', 'referral_code',
+            'timezone', 'two_way_auth', 'intro_check', 'app_intro_check',
+            'step', 'register_from_app', 'email_verified_at', 'company_name','apple_id','google_id']);
     }
 
     // appends
