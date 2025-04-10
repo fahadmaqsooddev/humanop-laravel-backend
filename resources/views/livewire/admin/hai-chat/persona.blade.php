108a1413cabef6f437129c3adc12a2dd017aa844
hamza2112 <farooqhamza94@gmail.com>
2025-04-10 13:03:31 +0500

    change the layout of the clusters section and add prompts of save and exit

--- resources/views/livewire/admin/hai-chat/persona.blade.php
+++ resources/views/livewire/admin/hai-chat/persona.blade.php
@@ -24,7 +24,7 @@
 
         <div class="card-header">
             <h5 class="text-orange setting-form-heading py-0">Name of Persona</h5>
-            <input type="text" class="form-control input-bg" id="chatDescription" wire:model.defer="persona_name" placeholder="Enter name of persona">
+            <input type="text" class="form-control input-bg change-input-form" id="chatDescription" wire:model.defer="persona_name" placeholder="Enter name of persona">
         </div>
 
         <div class="card-header">
@@ -39,7 +39,7 @@
 
         <div class="card-header">
             <h5 class="text-orange setting-form-heading py-0">CONNECT WITH HUMANOP APP?</h5>
-            <select class="form-control input-bg" id="human_app" wire:model.defer="human_op_app" onchange="alreadyExistsHumanApp(this)">
+            <select class="form-control input-bg change-input-form" id="human_app" wire:model.defer="human_op_app" onchange="alreadyExistsHumanApp(this)">
                 @if(empty($chat_bot_id))
                     <option value="">SELECT BRAIN FIRST</option>
                 @else
@@ -66,7 +66,7 @@
 
         <div class="card-body d-sm-flex pt-0 justify-content-end">
             <button style="padding: 10px 16px 10px 16px; border-radius: 7px;" wire:click="updateOrSave"
-                    class="mt-4 btn-sm-1 btn-md-3 btn-lg-5 float-end new-orange-button navButtonResponsive">
+                    class="mt-4 btn-sm-1 btn-md-3 btn-lg-5 float-end new-orange-button navButtonResponsive update-button">
                 update
                 <span wire:loading wire:target="updateOrSave" style="font-size: 8px;" class="swal2-loader">
                 </span>
