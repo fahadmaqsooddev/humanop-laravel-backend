4adc3492639f1f7706728a4f3b22aecf20925d6e
Muhammad Ali Butt <ab7464895@gmail.com>
2025-04-18 17:44:27 +0500

    eye icon added in reset passsword

--- resources/views/livewire/b2b/b2b-organizations/b2b-organization.blade.php
+++ resources/views/livewire/b2b/b2b-organizations/b2b-organization.blade.php
@@ -1,5 +1,27 @@
 @push('css')
+    <style>
 
+        .swal2-input.custom-input {
+            width: 100% !important;
+            max-width: 400px; /* prevents it from being too wide on large screens */
+            box-sizing: border-box;
+        }
+
+        .custom-confirm-btn {
+            background-color: #F2661C !important;
+            color: white !important;
+            border: none;
+        }
+
+        .custom-swal-btn {
+            background-color: #F2661C !important;
+            color: white !important;
+            border: none;
+            padding: 0.6rem 1.2rem;
+            border-radius: 5px;
+            font-weight: bold;
+        }
+    </style>
 @endpush
 <div>
     <div class="table-header-text">
@@ -12,7 +34,7 @@
                 <input type="email" name="email" wire:model.debounce="email"
                        class="form-control table-orange-color search-bar" placeholder="Search Email">
             </div>
-           
+
         </div>
     </div>
 
@@ -27,19 +49,19 @@
                 <th>Members</th>
                 <th>Candidates</th>
                 <th>Change Password</th>
-               
+
             </tr>
             </thead>
             <tbody>
-                
-                
-                
+
+
+
             @foreach($users as $user)
-            
+
                 <tr class="text-color-blue">
                     <td class="text-sm font-weight-normal ">{{$user['first_name'].' '.$user['last_name'] }} </td>
-                    <td class="text-sm font-weight-normal ">{{$user['email']}}</td> 
-                    <td class="text-sm font-weight-normal text-center">{{$user['gender']==0 ? 'Male':'FeMale'}}</td> 
+                    <td class="text-sm font-weight-normal ">{{$user['email']}}</td>
+                    <td class="text-sm font-weight-normal text-center">{{$user['gender']==0 ? 'Male':'FeMale'}}</td>
                     <td class="text-sm font-weight-normal text-center">
                         {{$user['member_count']}}
                     </td>
@@ -53,8 +75,8 @@
                             Reset Password
                         </a>
                     </td>
-                    
-                    
+
+
                 </tr>
             @endforeach
             </tbody>
@@ -66,189 +88,40 @@
 @push('js')
     <script src="../../assets/js/plugins/sweetalert.min.js"></script>
     <script>
-        function updateUserHaiChatStatus(id, name, checkbox, e) {
-            e.preventDefault();
-
-            // Store the current state of the checkbox
-            const isChecked = checkbox.checked;
-
-            // Reset checkbox to its original state temporarily
-            checkbox.checked = !isChecked;
-
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-primary m-2',
-                    cancelButton: 'btn bg-gradient-secondary m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            });
-
-            swalWithBootstrapButtons.fire({
-                title: '<span style="color: white;">Are you sure?</span>',
-                html: "<span style='color: white;'>Want to change HAI Chat Visibility for " + name + ".</span>",
-                showCancelButton: true,
-                confirmButtonText: 'Confirm',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // If confirmed, re-check the checkbox
-                    checkbox.checked = isChecked;
-                    // Trigger Livewire event
-                    window.livewire.emit('updateHaiChatVisibility', id);
-                } else {
-                    // If not confirmed, reset the checkbox to its original state
-                    checkbox.checked = !isChecked;
-                }
-            });
-        }
-        function updateUserEmailVerifiedStatus(id, name, checkbox, e) {
-            e.preventDefault();
-
-            // Store the current state of the checkbox
-            const isChecked = checkbox.checked;
-            const status = checkbox.id;
 
 
-
-            // Reset checkbox to its original state temporarily
-            checkbox.checked = !isChecked;
-
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-primary m-2',
-                    cancelButton: 'btn bg-gradient-secondary m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            });
-            let title = '';
-            let html = '';
-            if (status == 1) {
-        title = '<span style="color: white;">Are you sure?</span>';
-        html = "<span style='color: white;'>This email is already verified.</span>";
-    } else {
-        title = '<span style="color: white;">Are you sure?</span>';
-        html = "<span style='color: white;'>Want to verify the email of  " + name + "?</span>";
-    }
-            swalWithBootstrapButtons.fire({
-                title: title,
-                html: html,
-                showCancelButton: true,
-                confirmButtonText: 'Confirm',
-            }).then((result) => {
-                if (result.isConfirmed) {
-
-                    if(status!=1){
-                        checkbox.checked = isChecked;
-
-                    window.livewire.emit('updateEmailVerified', id);
-                    }
-
-                } else {
-
-                    checkbox.checked = !isChecked;
-                }
-            });
-        }
-
-
-        function adminLoggedInToUserAccount(id, firstName, lastName, identify) {
-
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-primary m-2',
-                    cancelButton: 'btn bg-gradient-secondary m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            })
-            swalWithBootstrapButtons.fire({
-                title: '<span style="color: white;">Are you sure?</span>',
-                html: "<span style='color: white;'>Want to log in as " + firstName + " " + lastName + "</span>",
-                showCancelButton: true,
-                confirmButtonText: 'Log in',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    localStorage.setItem('is_admin', true);
-                    window.livewire.emit('logInAdminAsUser', id, identify)
-                }
-            })
-        }
-
-        function changeUserToPractitioner(id, name, checkbox, e) {
-            e.preventDefault();
-
-            // Store the current state of the checkbox
-            const isChecked = checkbox.checked;
-
-            // Reset checkbox to its original state temporarily
-            checkbox.checked = !isChecked;
-
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-primary m-2',
-                    cancelButton: 'btn bg-gradient-secondary m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            });
-
-            swalWithBootstrapButtons.fire({
-                title: '<span style="color: white;">Are you sure?</span>',
-                html: "<span style='color: white;'>Want to Change Practitioner Status for " + name + ".</span>",
-                showCancelButton: true,
-                confirmButtonText: 'Confirm',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    // If confirmed, re-check the checkbox
-                    checkbox.checked = isChecked;
-                    // Trigger Livewire event
-                    window.livewire.emit('makePractitioner', id);
-                } else {
-                    // If not confirmed, reset the checkbox to its original state
-                    checkbox.checked = !isChecked;
-                }
-            });
-        }
-
-        // function deleteClientProfile(id, name) {
-
-        //     const swalWithBootstrapButtons = Swal.mixin({
-        //         customClass: {
-        //             confirmButton: 'btn bg-gradient-danger m-2',
-        //             cancelButton: 'btn bg-gradient-secondary m-2',
-        //         },
-        //         buttonsStyling: false,
-        //         background: '#3442b4',
-        //     })
-        //     swalWithBootstrapButtons.fire({
-        //         title: '<span style="color: white;">Are you sure?</span>',
-        //         html: "<span style='color: white;'>Want to delete " + name + " Profile</span>",
-        //         showCancelButton: true,
-        //         confirmButtonText: 'Delete',
-        //     }).then((result) => {
-        //         if (result.isConfirmed) {
-        //             window.livewire.emit('deleteClientProfile', id)
-        //         }
-        //     })
-        // }
-
-
-        function resetPassword(id, name) {
+function resetPassword(id, name) {
     Swal.fire({
         title: `<span style="color:white;">Reset Password</span>`,
         html: `
-            <input type="password" id="newPassword" class="swal2-input" placeholder="Enter new password">`,
-        background: '#3442b4',
+            <div style="position: relative; display: flex; justify-content: center;">
+                <input type="password" id="newPassword" class="swal2-input custom-input" placeholder="Enter new password">
+                <span id="togglePassword" style="position: absolute; right: 40px; top: 60%; transform: translateY(-50%); cursor: pointer; color: #999;">
+                    <i class="fa fa-eye" id="eyeIcon"></i>
+                </span>
+            </div>
+        `,
+        background: '#1c365e',
         color: 'white',
         showCancelButton: true,
         confirmButtonText: 'Reset',
         cancelButtonText: 'Cancel',
         customClass: {
-            confirmButton: 'btn bg-gradient-danger m-2',
+            confirmButton: 'btn custom-confirm-btn m-2',
             cancelButton: 'btn bg-gradient-secondary m-2',
         },
         buttonsStyling: false,
+        didOpen: () => {
+            const toggle = Swal.getPopup().querySelector('#togglePassword');
+            const input = Swal.getPopup().querySelector('#newPassword');
+            const icon = Swal.getPopup().querySelector('#eyeIcon');
+
+            toggle.addEventListener('click', () => {
+                const type = input.type === 'password' ? 'text' : 'password';
+                input.type = type;
+                icon.className = type === 'password' ? 'fa fa-eye' : 'fa fa-eye-slash';
+            });
+        },
         preConfirm: () => {
             const password = Swal.getPopup().querySelector('#newPassword').value;
             if (!password) {
@@ -259,65 +132,34 @@
     }).then((result) => {
         if (result.isConfirmed) {
             const password = result.value.password;
-            // Emit to Livewire
             window.livewire.emit('resetPassword', id, password);
         }
     });
 }
 
 
-        function deleteBulkClient(){
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-danger m-2',
-                    cancelButton: 'btn bg-gradient-secondary m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            })
-            swalWithBootstrapButtons.fire({
-                title: '<span style="color: white;">Are you sure?</span>',
-                html: "<span style='color: white;'>Want to delete Clients </span>",
-                showCancelButton: true,
-                confirmButtonText: 'Delete',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.livewire.emit('bulkDelete')
-                }
-            })
-        }
-
-        function displayChatBotPublishMessage(event){
-            event.preventDefault();
-
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn bg-gradient-danger m-2',
-                },
-                buttonsStyling: false,
-                background: '#3442b4',
-            })
-            swalWithBootstrapButtons.fire({
-                // title: '<span style="color: white;">Publish Chat Bot first to enable Hai chat.</span>',
-                html: "<h4 style='color: white;'>Publish Chat Bot first to enable Hai chat.</h4>",
-                // showCancelButton: true,
-                confirmButtonText: 'Close',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    event.target.checked = false;
-                }
-            })
-
-        }
-
-
-        function changeUserMemberShip(e, user_id) {
-
-            window.Livewire.emit('changeUserMemberShip', e.options.selectedIndex, user_id);
-        }
-
-        
     </script>
+{{--    <script>--}}
+{{--        window.addEventListener('swal:error', event => {--}}
+{{--            Swal.fire({--}}
+{{--                icon: 'error',--}}
+{{--                text: event.detail.message,--}}
+{{--                background: '#1c365e',--}}
+{{--                color: '#f6ba81',--}}
+{{--            });--}}
+{{--        });--}}
+
+{{--        window.addEventListener('swal:success', event => {--}}
+{{--            Swal.fire({--}}
+{{--                icon: 'success',--}}
+{{--                text: event.detail.message,--}}
+{{--                background: '#1c365e',--}}
+{{--                color: '#f6ba81',--}}
+{{--            });--}}
+{{--        });--}}
+{{--    </script>--}}
+
+
     <script>
         window.addEventListener('swal:error', event => {
             Swal.fire({
@@ -325,17 +167,28 @@
                 text: event.detail.message,
                 background: '#1c365e',
                 color: '#f6ba81',
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'custom-swal-btn'
+                },
+                buttonsStyling: false
             });
         });
-    
+
         window.addEventListener('swal:success', event => {
             Swal.fire({
                 icon: 'success',
                 text: event.detail.message,
                 background: '#1c365e',
                 color: '#f6ba81',
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'custom-swal-btn'
+                },
+                buttonsStyling: false
             });
         });
     </script>
-    
+
+
 @endpush
