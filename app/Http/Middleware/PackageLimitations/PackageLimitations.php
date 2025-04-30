e085070ba2ecd86a616289a79dc7a8e2e0c2f400
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-04-30 11:28:15 +0500

    check member

--- app/Http/Middleware/PackageLimitations/PackageLimitations.php
+++ app/Http/Middleware/PackageLimitations/PackageLimitations.php
@@ -47,8 +47,8 @@ class PackageLimitations
                 $getMemberInvites = UserCandidateInvite::where('company_id', $user_id)->where('role', Admin::IS_TEAM_MEMBER)->get();
 
 
-                dd(count($getExistingMembers), $limitations['no_of_team_members'], count($getMemberInvites));
-                
+                dd(count($getExistingMembers), count($getMemberInvites));
+
                 if ((count($getExistingMembers) < (int)$limitations['no_of_team_members']) && (count($getMemberInvites) < (int)$limitations['no_of_team_members'])) {
 
                     return $next($request);
