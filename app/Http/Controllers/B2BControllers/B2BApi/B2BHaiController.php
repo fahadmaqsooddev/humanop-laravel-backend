4cf0f17faef78b0a473c866af998c270d0195ae5
hamza2112 <farooqhamza94@gmail.com>
2025-05-02 22:27:44 +0500

    user intentions are added

--- app/Http/Controllers/B2BControllers/B2BApi/B2BHaiController.php
+++ app/Http/Controllers/B2BControllers/B2BApi/B2BHaiController.php
@@ -103,11 +103,11 @@ class B2BHaiController extends Controller
 
                 $user_name = $user['first_name'];
 
-//                $user_intentions = $user?->businessIntentions?->pluck('description')->toArray();
+                $user_intentions = $user?->businessIntentions?->pluck('description')->toArray();
 
 //                $interval_life = User::userIntervalOfLife($user['date_of_birth']);
 
-                $body = ["query" => $request->input('question'), 'temperature' => $setting['temperature'], 'max_tokens' => $setting['max_token'], 'file_name' => $activeChatAndEmbedding['file_name'], 'prompt_folder' => $chat_bot['name'], 'total_chunks' => $setting['chunk'], 'gpt_model' => 'sonnet','user_grid' => $user_grid ?? [], 'dislike' => $request->input('is_repeat_answer'), 'loc' => $subFolder, 'user_name' => $user_name, 'user_id' => (int)Helpers::getUser()->id];
+                $body = ["query" => $request->input('question'), 'temperature' => $setting['temperature'], 'max_tokens' => $setting['max_token'], 'file_name' => $activeChatAndEmbedding['file_name'], 'prompt_folder' => $chat_bot['name'], 'total_chunks' => $setting['chunk'], 'gpt_model' => 'sonnet','user_grid' => $user_grid ?? [], 'dislike' => $request->input('is_repeat_answer'), 'loc' => $subFolder, 'user_name' => $user_name, 'user_id' => (int)Helpers::getUser()->id, 'user_intentions' => $user_intentions];
 
                 $aiReply = GuzzleHelpers::sendRequestFromGuzzle('post', 'b2b-llm-model', $body);
 
