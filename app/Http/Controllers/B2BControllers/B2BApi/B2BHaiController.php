0ae542b1f1b6b6d3fbfe158c8a94f55fa01d9880
hamza2112 <farooqhamza94@gmail.com>
2025-05-02 19:48:38 +0500

    log

--- app/Http/Controllers/B2BControllers/B2BApi/B2BHaiController.php
+++ app/Http/Controllers/B2BControllers/B2BApi/B2BHaiController.php
@@ -20,6 +20,7 @@ use App\Models\HAIChai\LlmModel;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class B2BHaiController extends Controller
 {
@@ -110,6 +111,8 @@ class B2BHaiController extends Controller
                 $body = ["query" => $request->input('question'), 'temperature' => $setting['temperature'], 'max_tokens' => $setting['max_token'], 'file_name' => $activeChatAndEmbedding['file_name'], 'prompt_folder' => $chat_bot['name'], 'total_chunks' => $setting['chunk'], 'gpt_model' => 'sonnet','user_grid' => $user_grid ?? [], 'dislike' => $request->input('is_repeat_answer'), 'loc' => $subFolder, 'user_name' => $user_name, 'user_id' => (int)Helpers::getUser()->id, 'total_count' => "10"];
 
                 $aiReply = GuzzleHelpers::sendRequestFromGuzzle('post', 'b2b-llm-model', $body);
+
+                Log::info(['ai re' => $aiReply]);
 
                 $llm_prompt = OpenRouterHelper::addUserDetailsIntoPrompt(Helpers::getUser()->id, $aiReply['prompt']);
 
