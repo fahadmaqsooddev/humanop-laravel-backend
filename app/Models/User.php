62eee5dbb813ff6b58cca34ca50b2300b36ef29d
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-05-05 13:41:14 +0500

    null condition add

--- app/Models/User.php
+++ app/Models/User.php
@@ -656,7 +656,7 @@ class User extends Authenticatable implements JWTSubject
 
         $data['is_admin'] = !empty($is_admin) ? Admin::IS_B2B : Admin::IS_CUSTOMER;
 
-        $data['company_name'] = '';
+        $data['company_name'] = null;
 
         $data['status'] = 1;
 
