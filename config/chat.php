61b839e1c1ae43c3672d848216e78817bc8fd411
hamza2112 <55729330+Hamza2112@users.noreply.github.com>
2025-05-06 13:56:07 +0500

    change the production server url for HAi

--- config/chat.php
+++ config/chat.php
@@ -6,5 +6,5 @@ return [
      'api' => 'http://54.227.7.149:8000/llm-response',
     'dev_api_urls' => 'http://54.227.7.149:8000/',
     'staging_api_urls' => 'http://44.201.128.253:8000/',
-    'live_api_urls' => 'http://18.234.162.68:8000/',
+    'live_api_urls' => 'http://18.207.0.192:8000/',
 ];
