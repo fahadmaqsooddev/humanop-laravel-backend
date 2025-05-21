fc0b5e613877e35e4805a226cc1c100fdc17956d
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-05-21 11:38:16 +0500

    b2c pricing plan add

--- app/Http/Livewire/Admin/PricingPlan/CreatePricingPlan.php
+++ app/Http/Livewire/Admin/PricingPlan/CreatePricingPlan.php
@@ -1,21 +1,19 @@
 <?php
 
-namespace App\Http\Livewire\B2b\B2bPricingPlan;
+namespace App\Http\Livewire\Admin\PricingPlan;
 
+use App\Enums\Admin\Admin;
+use App\Helpers\Helpers;
+use App\Models\Client\Plan\Plan;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Stripe\Price;
-use App\Helpers\Helpers;
-use App\Enums\Admin\Admin;
-use App\Models\Client\Plan\Plan;
 use Stripe\Product;
 use Stripe\Stripe;
 
-use Illuminate\Support\Facades\DB;
-
 class CreatePricingPlan extends Component
 {
-
-    public $plan_name, $price, $plan_type, $team_members, $plans, $plan_id;
+    public $plan_name, $price, $plan_type, $plans, $plan_id;
 
     protected $listeners = ['activeInactivePlanModal'];
 
@@ -23,105 +21,17 @@ class CreatePricingPlan extends Component
         'plan_name' => 'required',
         'price' => 'required',
         'plan_type' => 'required',
-        'team_members' => 'required',
     ];
 
     protected $messages = [
         'plan_name.required' => 'Plan name is required.',
         'price.required' => 'Price is required.',
         'plan_type.required' => 'Plan type is required.',
-        'team_members.required' => 'Please specify the number of team members.',
     ];
 
     public function getPlans()
     {
-        $this->plans = Plan::getB2BPlans();
-    }
-
-    public function updatePlanModal($planId, $planName, $planPrice, $noOfMembers, $planType)
-    {
-
-        $this->plan_id = $planId;
-
-        $this->plan_name = $planName;
-
-        $this->price = $planPrice;
-
-        $this->team_members = $noOfMembers;
-
-        $this->plan_type = $planType;
-
-    }
-
-    public function activeInactivePlanModal($planId)
-    {
-        DB::beginTransaction();
-        try {
-
-            Stripe::setApiKey(config('cashier.secret'));
-
-            $getPlan = Plan::getSingleB2BPlan($planId);
-
-            $price = Price::retrieve($getPlan['plan_id']);
-
-            $productId = $price->product;
-
-            if ($getPlan && $getPlan['status'] == 1) {
-
-                Price::update($getPlan['plan_id'], [
-                    'active' => false,
-                ]);
-
-                Product::update($productId, [
-                    'active' => false,
-                ]);
-
-                $getPlan->update(['status' => 0]);
-
-                DB::commit();
-
-                session()->flash('success', 'Plan Inactive successfully!');
-
-            } else {
-
-                $b2bActivePlan = Plan::activeb2BPlans();
-
-                if ($b2bActivePlan < 4) {
-
-                    Price::update($getPlan['plan_id'], [
-                        'active' => true,
-                    ]);
-
-                    Product::update($productId, [
-                        'active' => true,
-                    ]);
-
-                    $getPlan->update(['status' => 1]);
-
-                    DB::commit();
-
-                    session()->flash('success', 'Plan activated successfully!');
-
-                } else {
-
-                    DB::rollBack();
-
-                    session()->flash('error', 'You can only have a maximum of 4 active plans.');
-
-                }
-
-            }
-
-        } catch (\Exception $exception) {
-
-            DB::rollBack();
-
-            session()->flash('error', 'Something went wrong: ' . $exception->getMessage());
-
-            return Helpers::serverErrorResponse($exception->getMessage());
-
-        }
-
+        $this->plans = Plan::getB2CPlans();
     }
 
     public function submitForm()
@@ -152,13 +62,13 @@ class CreatePricingPlan extends Component
                 'interval_count' => $price['recurring']['interval_count'],
                 'price' => $this->price,
                 'currency' => $price['currency'],
-                'plan_type' => Admin::B2B_PLAN,
-                'no_of_team_members' => $this->team_members,
+                'plan_type' => Admin::B2C_PLAN,
             ];
 
             Plan::storePlan($this->data);
 
             DB::commit();
+
             $this->resetForm();
 
             session()->flash('success', 'Plan created successfully!');
@@ -167,12 +77,101 @@ class CreatePricingPlan extends Component
 
             DB::rollBack();
 
-            session()->flash('error', 'Something went wrong: ' . $exception->getMessage());
+            session()->flash('error', $exception->getMessage());
 
             return Helpers::serverErrorResponse($exception->getMessage());
         }
 
     }
+
+    public function updatePlanModal($planId, $planName, $planPrice, $noOfMembers, $planType)
+    {
+
+        $this->plan_id = $planId;
+
+        $this->plan_name = $planName;
+
+        $this->price = $planPrice;
+
+        $this->team_members = $noOfMembers;
+
+        $this->plan_type = $planType;
+
+    }
+
+    public function activeInactivePlanModal($planId)
+    {
+        DB::beginTransaction();
+
+        try {
+
+            Stripe::setApiKey(config('cashier.b2c_secret'));
+
+            $getPlan = Plan::getSingleB2BPlan($planId);
+
+            $price = Price::retrieve($getPlan['plan_id']);
+
+            $productId = $price->product;
+
+            if ($getPlan && $getPlan['status'] == 1) {
+
+                Price::update($getPlan['plan_id'], [
+                    'active' => false,
+                ]);
+
+                Product::update($productId, [
+                    'active' => false,
+                ]);
+
+                $getPlan->update(['status' => 0]);
+
+                DB::commit();
+
+                session()->flash('success', 'Plan Inactive successfully!');
+
+            } else {
+
+                $b2bActivePlan = Plan::activeb2BPlans();
+
+//                if ($b2bActivePlan < 4) {
+
+                    Price::update($getPlan['plan_id'], [
+                        'active' => true,
+                    ]);
+
+                    Product::update($productId, [
+                        'active' => true,
+                    ]);
+
+                    $getPlan->update(['status' => 1]);
+
+                    DB::commit();
+
+                    session()->flash('success', 'Plan activated successfully!');
+
+//                } else {
+//
+//                    DB::rollBack();
+//
+//                    session()->flash('error', 'You can only have a maximum of 4 active plans.');
+//
+//                }
+
+            }
+
+        } catch (\Exception $exception) {
+
+            DB::rollBack();
+
+            session()->flash('error', 'Something went wrong: ' . $exception->getMessage());
+
+            return Helpers::serverErrorResponse($exception->getMessage());
+
+        }
+
+    }
+
+
 
     public function updateB2bPlan()
     {
@@ -221,16 +220,15 @@ class CreatePricingPlan extends Component
 
     public function resetForm()
     {
-        $this->reset(['plan_name', 'price', 'plan_type', 'team_members']);
+        $this->reset(['plan_name', 'price', 'plan_type']);
     }
 
     public function render()
     {
-
         $this->getPlans();
 
-        return view('livewire.b2b.b2b-pricing-plan.create-pricing-plan', [
-            'plans' => $this->plans
-        ]);
+        return view('livewire.admin.pricing-plan.create-pricing-plan', ['plans' => $this->plans]);
+
     }
 }
+
---
+++ resources/views/admin-dashboards/pricing-plans/index.blade.php
@@ -0,0 +1,58 @@
+@extends('user_type.auth', ['parentFolder' => 'dashboards', 'childFolder' => 'none'])
+<style>
+    .modal-close-btn {
+        background: #f2661c;
+        border: none;
+        color: white;
+        font-weight: bold;
+        font-size: x-large;
+        float:right;
+        border-radius: 3px;
+        padding: 0px 10px 1px 10px;
+    }
+    .pagination{
+        float:right;
+        margin-right:24px ;
+    }
+    .page-link {
+        background: none !important;
+    }
+    .page-link:hover{
+        background: #f2661c !important;
+        color:white !important;
+    }
+
+    .page-item.active .page-link {
+        background: #f2661c !important;
+        color: white !important;
+        border-color: #f2661c !important;
+    }
+
+    .table-text-color{
+        color: #1c365e !important;
+    }
+
+    .dataTable-table th a{
+        color: #1c365e !important;
+    }
+
+</style>
+@section('content')
+    <div class="row mt-4 container-fluid">
+        <div class="col-12">
+
+            @livewire('admin.pricing-plan.create-pricing-plan')
+
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ URL::asset('assets/js/plugins/datatables.js') }}"></script>
+    <script>
+        const dataTableSearch = new simpleDatatables.DataTable("#datatable-search", {
+            searchable: true,
+            fixedHeight: true
+        });
+    </script>
+@endpush
