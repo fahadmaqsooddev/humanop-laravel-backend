56f83a2fd94f176acd68c1096c7e9c6a6c917dfb
hamza2112 <55729330+Hamza2112@users.noreply.github.com>
2025-06-10 18:06:25 +0500

    sync user info with the HAi on B2B signup

--- app/Http/Controllers/Api/ClientController/AuthController.php
+++ app/Http/Controllers/Api/ClientController/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\ClientController;
 
 use App\Enums\Admin\Admin;
+use App\Helpers\HaiChat\HaiChatHelpers;
 use App\Helpers\Helpers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\CheckCandidate;
@@ -137,6 +138,8 @@ class AuthController extends Controller
                     }
 
                     Point::addPoints(Admin::FREEMIUM_CREDITS, $user);
+
+                    HaiChatHelpers::syncUserRecordWithHAi($user);
 
                     Helpers::createCustomerAndSubscriptionOnStripe($user);
 
