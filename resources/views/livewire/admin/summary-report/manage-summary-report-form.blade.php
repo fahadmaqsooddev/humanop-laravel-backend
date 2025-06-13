982907310d4c59dca6f4f525a28f228e02ec8517
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-06-13 17:50:10 +0500

    manage-summary-report-form

--- resources/views/livewire/admin/summary-report/manage-summary-report-form.blade.php
+++ resources/views/livewire/admin/summary-report/manage-summary-report-form.blade.php
@@ -1,12 +1,11 @@
 @push('css')
-    <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/43.2.0/ckeditor5.css">
     <style>
         .ck-editor__editable_inline {
-            background-color: #eaf3ff ; /* Example: Change this to your desired background color */
+            background-color: #eaf3ff; /* Example: Change this to your desired background color */
         }
 
         .ck-editor__editable {
-            background-color: #eaf3ff  !important;
+            background-color: #eaf3ff !important;
         }
 
         .ck-editor {
@@ -22,7 +21,7 @@
             z-index: 1050 !important;
         }
 
-        .ck > p > a{
+        .ck > p > a {
             color: blue !important;
         }
 
@@ -33,14 +32,14 @@
     <input type="hidden" wire:model.defer="select_code.id">
     <div class="row">
         <div class="col-12">
-            <label class="form-label text-white">Name</label>
+            <label class="form-label">Name</label>
             <div class="input-group">
                 <input style="color: #0f1534; background-color: #eaf3ff " name="name"
                        class="form-control" type="text" wire:model.defer="select_code.name">
             </div>
         </div>
         <div class="col-12 mt-4">
-            <label class="form-label text-white">Public Name</label>
+            <label class="form-label">Public Name</label>
             <div class="input-group">
                 <input style="color: #0f1534; background-color: #eaf3ff " name="public_name"
                        class="form-control" type="text" wire:model.defer="select_code.public_name"
@@ -48,7 +47,7 @@
             </div>
         </div>
         <div class="col-12 mt-4">
-            <label class="form-label text-white">Code</label>
+            <label class="form-label">Code</label>
             <div class="input-group">
                 <input style="color: #0f1534; background-color: #eaf3ff " name="code"
                        class="form-control" type="text" wire:model.defer="select_code.code"
@@ -56,111 +55,50 @@
             </div>
         </div>
         <div class="col-12 mt-4">
-            <label class="form-label text-white">Type</label>
+            <label class="form-label">Type</label>
             <div class="input-group">
                 <input style="color: #0f1534; background-color: #eaf3ff " name="type"
                        class="form-control" type="text" wire:model.defer="select_code.type"
                        placeholder="Alec">
             </div>
         </div>
-        {{-- <div class="row"> --}}
         <div class="col-12 mt-4">
-            <label class="form-label text-white">Text</label>
+            <label class="form-label">Text</label>
             <div class="input-group w-100" wire:ignore>
-
-            <textarea class="form-control table-header-text" id="editor" rows="10" cols="10"
-            name="text"
-            wire:model.defer="select_code.text">{{ $select_code['text'] }}</textarea>
+                <textarea class="form-control table-header-text" id="summernote" rows="10" cols="10" name="text"
+                          wire:model.defer="select_code.text">{{ $select_code['text'] }}</textarea>
             </div>
         </div>
-    {{-- </div> --}}
     </div>
     <button class="btn btn-sm float-end mt-6 mb-0 text-white" style="background-color: #1B3A62 ">Update Summary</button>
 </form>
 
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-<script type="importmap">
-    {
-        "imports": {
-            "ckeditor5": "https://cdn.ckeditor.com/ckeditor5/43.2.0/ckeditor5.js",
-            "ckeditor5/": "https://cdn.ckeditor.com/ckeditor5/43.2.0/"
-        }
-    }
-
-
-
-
-    </script>
-    <script src="{{ URL::asset('assets/js/plugins/datatables.js') }}"></script>
+@push('javascript')
+    <!-- jQuery & Summernote -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../../assets/js/plugins/sweetalert.min.js"></script>
-<script type="text/javascript">
-    document.addEventListener('livewire:load', function () {
-        const summernoteElement = $('#editor');
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 
-        summernoteElement.summernote({
-            height: 300,
-            callbacks: {
-                onChange: function(contents, $editable) {
-                @this.set('select_code.text', contents);
-                }
-            }
-        });
+    <script>
+        document.addEventListener('livewire:load', function () {
 
-        Livewire.on('contentUpdated', function(content) {
-            summernoteElement.summernote('code', content);
-        });
-    });
-
-</script>
-
-
-    <script type="module">
-        import {
-            ClassicEditor,
-            Essentials,
-            Paragraph,
-            Bold,
-            Italic,
-            Font,
-            List,
-            Link,
-            AutoLink
-        } from 'ckeditor5';
-
-        // Function to initialize CKEditor for a specific textarea by ID
-        let editorInstance;
-        const editorElement = document.getElementById('editor');
-        if (editorElement && !editorElement.classList.contains('ck-editor')) { // Check if not already initialized
-            ClassicEditor
-                .create(editorElement, {
-                    plugins: [Essentials, Paragraph, Bold, Italic, Font, List, Link, AutoLink],
-                    toolbar: [
-                        'undo', 'redo', '|', 'bold', 'italic', '|',
-                        'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
-                        'bulletedList', 'numberedList', 'link'  // Add list options to toolbar
-                    ]
-                })
-                .then(editor => {
-                    editor.model.document.on('change:data', () => {
-                    @this.set('select_code.text', editor.getData());
-                    })
-                    Livewire.on('contentUpdated', content => {
-                        editor.setData(content); // Set new content into CKEditor
-                    });
-                    editorInstance = editor;
-                })
-                .catch(error => {
-                    console.error(error);
+            function initSummernote() {
+                $('#summernote').summernote({
+                    height: 200,
+                    callbacks: {
+                        onChange: function (contents, $editable) {
+                        @this.set('select_code.text', contents);
+                        }
+                    }
                 });
-
-        }
-        $('.createForm').on('click', function () {
-            if (editorInstance) {
-                editorInstance.setData('');
             }
+
+            initSummernote();
+
+            Livewire.hook('message.processed', () => {
+                initSummernote();
+            });
+
         });
-
-
     </script>
+@endpush
