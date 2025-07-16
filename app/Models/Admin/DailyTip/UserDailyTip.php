2e8d4243e5c8f42a0d2a96187a879111b2ae1da5
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-07-16 19:59:43 +0500

    points add for core plan

--- app/Models/Admin/DailyTip/UserDailyTip.php
+++ app/Models/Admin/DailyTip/UserDailyTip.php
@@ -94,13 +94,13 @@ class UserDailyTip extends Model
 
             HumanOpPoints::addPointsAfterCompleteDailyTip($user);
 
-//            $message = 'Your New Daily Tip';
-//
-//            event(new NewDailyTip($user['id'], 'new daily tip', $message));
-//
-//            Helpers::OneSignalApiUsed($user['id'], 'new daily tip', $message);
-//
-//            Notification::createNotification('Daily Tip', $message, $user['device_token'], $user['id'], 1, Admin::DAILY_TIP_NOTIFICATION, Admin::B2C_NOTIFICATION);
+            $message = 'Your New Daily Tip';
+
+            event(new NewDailyTip($user['id'], 'new daily tip', $message));
+
+            Helpers::OneSignalApiUsed($user['id'], 'new daily tip', $message);
+
+            Notification::createNotification('Daily Tip', $message, $user['device_token'], $user['id'], 1, Admin::DAILY_TIP_NOTIFICATION, Admin::B2C_NOTIFICATION);
 
         }
 
