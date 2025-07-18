31a55b1a0095eff89d422c81c89e5e73e20f30da
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-07-18 15:18:51 +0500

    api name change

--- routes/client_apis/dashboard/dashboard_api.php
+++ routes/client_apis/dashboard/dashboard_api.php
@@ -38,9 +38,9 @@ Route::group(['middleware' => ['checkUser']], function () {
 
 
     Route::get('all-companies', 'DashboardController@allCompanies');
-    Route::get('check-future-consideration-member-share-data','DashboardController@checkFutureConsiderationShareData');
-    Route::post('future-consideration-member-share-data','DashboardController@futureConsiderationShareData');
-    Route::post('future-consideration-member-not-share-data','DashboardController@futureConsiderationNotShareData');
+    Route::get('check-future-consideration-share-data','DashboardController@checkFutureConsiderationShareData');
+    Route::post('future-consideration-share-data','DashboardController@futureConsiderationShareData');
+    Route::post('future-consideration-not-share-data','DashboardController@futureConsiderationNotShareData');
 
    Route::get('hai-chat-status', 'DashboardController@haiChatStatus');
 });
