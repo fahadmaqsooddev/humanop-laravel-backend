58d478e416637418fe0f3bf7af41b52722c31f23
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-07-22 13:18:53 +0500

    trial_time rename

--- database/migrations/2025_07_22_074152_add_trial_time_column_users_table.php
+++ database/migrations/2025_07_22_074152_add_trial_time_column_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
 
-            $table->dateTime('trail_time')->nullable();
+            $table->dateTime('trial_time')->nullable();
 
         });
     }
@@ -29,7 +29,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
 
-            $table->dropColumn('trail_time');
+            $table->dropColumn('trial_time');
 
         });
     }
