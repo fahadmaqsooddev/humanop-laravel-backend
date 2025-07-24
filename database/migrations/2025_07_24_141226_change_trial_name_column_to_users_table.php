ee7785d6b7cdaa70ba138d32dab4f78883476eff
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-07-24 19:55:44 +0500

    aa

--- database/migrations/2025_07_24_141226_change_trial_name_column_to_users_table.php
+++ database/migrations/2025_07_24_141226_change_trial_name_column_to_users_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
 
             $table->dropColumn('trial_day');
-            $table->dropColumn('trial_time');
+//            $table->dropColumn('trial_time');
 
         });
 
