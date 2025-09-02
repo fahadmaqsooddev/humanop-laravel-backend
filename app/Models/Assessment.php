2da2ff33d736294b2f1f45da67f9bd7717b7fbdf
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-09-02 18:38:11 +0500

    check data

--- app/Models/Assessment.php
+++ app/Models/Assessment.php
@@ -944,6 +944,7 @@ class Assessment extends Model
             return array_search($a, $style) <=> array_search($b, $style);
         });
 
+        dd($data);
         $styleCodes = CodeDetail::getStylePublicNames($data);
 
         $allStyles = PdfGenerate::createGenerateFile($assessment['id'], $assessment['users']['id'], $styleCodes, $data);
