8f93c5548c4eefa0f11f7d3d238d8b12a2448392
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-09-11 23:27:31 +0500

    try catch commit

--- app/Http/Controllers/Api/ClientController/DashboardController.php
+++ app/Http/Controllers/Api/ClientController/DashboardController.php
@@ -291,7 +291,7 @@ class DashboardController extends Controller
     public function coreStats(Request $request)
     {
 
-        try {
+//        try {
 
             $assessment = Assessment::singleAssessmentFromId($request->input('assessment_id', null));
 
@@ -299,10 +299,10 @@ class DashboardController extends Controller
 
             return Helpers::successResponse('core stats', $coreState);
 
-        } catch (\Exception $exception) {
-
-            return Helpers::serverErrorResponse($exception->getMessage());
-        }
+//        } catch (\Exception $exception) {
+//
+//            return Helpers::serverErrorResponse($exception->getMessage());
+//        }
     }
 
     public function dailyTipRead()
