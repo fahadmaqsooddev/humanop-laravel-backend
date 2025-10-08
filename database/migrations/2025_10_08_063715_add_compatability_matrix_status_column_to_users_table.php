f011fb3ef786314e74ab2cea99031b4265747704
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-10-08 11:55:35 +0500

    compatability matrx hide show api has been created

---
+++ database/migrations/2025_10_08_063715_add_compatability_matrix_status_column_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->integer('compatability_matrix_status')->default(2)->comment('1: Hide, 2: Show');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->dropColumn('compatability_matrix_status');
+
+        });
+    }
+};
