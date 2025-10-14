7444fb8f32f9a3f19603dc0d8278bce5d3b59d79
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-10-14 14:21:41 +0500

    Revert "core state api has been changed according to beta breaker"
    This reverts commit 4838e6d7816b9e64b4196f7b3ee612071ad1be89.

--- app/Models/Assessment.php
+++ app/Models/Assessment.php
@@ -863,21 +863,11 @@ class Assessment extends Model
             return array_search($a, $style) <=> array_search($b, $style);
         });
 
-        if (Helpers::getWebUser()['beta_breaker_club'] == Admin::BETA_BREAKER_CLUB){
+        $topFour = array_slice($data, 0, 3, true);
 
-            $styleCodes = CodeDetail::getStylePublicNames($data);
+        $styleCodes = CodeDetail::getStylePublicNames($topFour);
 
-            $allStyles = PdfGenerate::createGenerateFile($assessment['id'], $assessment['users']['id'], $styleCodes, $data);
-
-        }else{
-
-            $topFour = array_slice($data, 0, 3, true);
-
-            $styleCodes = CodeDetail::getStylePublicNames($topFour);
-
-            $allStyles = PdfGenerate::createGenerateFile($assessment['id'], $assessment['users']['id'], $styleCodes, $topFour);
-
-        }
+        $allStyles = PdfGenerate::createGenerateFile($assessment['id'], $assessment['users']['id'], $styleCodes, $topFour);
 
         $data = [];
 
