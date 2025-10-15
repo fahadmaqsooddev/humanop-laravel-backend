1c48edcddf6b379a5487112bd73fe795188c5731
AhtashamKhalid <131809011+AhtashamKhalid@users.noreply.github.com>
2025-10-15 13:45:48 +0500

    urls add in cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://dev.humanoptech.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
