d533fd4f95041abaa5cf683300cfb33bb4e4746c
Muhammad Hamza <hamza.waheed00777@gmail.com>
2025-10-21 16:15:00 +0500

    Update BroadcastServiceProvider.php

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,10 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes();
+        Broadcast::routes([
+            'middleware' => ['auth:api'], // <-- JWT-guarded
+            'prefix'     => 'broadcasting'
+        ]);
 
         require base_path('routes/channels.php');
     }
