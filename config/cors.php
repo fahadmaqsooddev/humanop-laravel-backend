546c0b8664ef1eb34452af32f94fc857765aeb6b
M Ahtasham <mahtasham060@gmail.com>
2025-10-21 17:46:07 +0500

    client url changed

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://dev.humanop.com','http://localhost:3000'],
+    'allowed_origins' => ['https://staging.humanop.com','http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
