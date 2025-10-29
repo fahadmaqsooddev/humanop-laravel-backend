df1424fda0f3038c95b53b1cbc41593d6d634b77
M Ahtasham <mahtasham060@gmail.com>
2025-10-29 17:14:38 +0500

    clear.sh file updated

--- clear.sh
+++ clear.sh
@@ -1,18 +1,29 @@
 echo "Deploy script started"
-sudo -u www-data php artisan config:clear
-sudo -u www-data php artisan cache:clear
-sudo -u www-data php artisan view:clear
-sudo -u www-data php artisan clear-compiled
-sudo -u www-data php artisan event:clear
+
+# Install deps first so autoload & vendors are up to date
+sudo -u www-data composer -n install --prefer-dist --no-progress --no-interaction
+sudo -u www-data composer -n dump-autoload -o
+
+# Clear all possible stale state
 sudo -u www-data php artisan optimize:clear
-sudo -u www-data php artisan optimize
+sudo -u www-data php artisan cache:clear
+sudo -u www-data php artisan config:clear
+sudo -u www-data php artisan route:clear
+sudo -u www-data php artisan view:clear
+sudo -u www-data php artisan event:clear
+sudo -u www-data php artisan clear-compiled
+
+# DB changes
+sudo -u www-data php artisan migrate --force
+
+# Rebuild caches (if desired for speed)
+sudo -u www-data php artisan config:cache
+sudo -u www-data php artisan route:cache
 sudo -u www-data php artisan view:cache
 sudo -u www-data php artisan event:cache
-sudo -u www-data php artisan config:cache
-sudo -u www-data composer -n dump-autoload -o
+
+# Optional feel-good :)
 sudo -u www-data php artisan inspire
-sudo -u www-data php artisan migrate
-sudo -u www-data composer -n install
-sudo -u www-data php artisan route:clear
+
 echo "Deploy script finished execution"
-#php artisan db:seed --class=CreditPlanSeeder
+# sudo -u www-data php artisan db:seed --class=CreditPlanSeeder
