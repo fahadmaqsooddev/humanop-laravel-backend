cc32954b4ee6a88b5615bb21b00c773adf506c37
Muhammad Hamza <hamza.waheed00777@gmail.com>
2025-10-29 21:22:47 +0500

    Update AuthController.php

--- app/Http/Controllers/Api/ClientController/AuthController.php
+++ app/Http/Controllers/Api/ClientController/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
     public function __construct(SnsServices $sns, private FreemiumEnrollmentService $freemiumService)
     {
         $this->middleware('auth:api')->except(['resendOtpCode', 'verifyOtpCode', 'SendInvite', 'loginClient', 'forgotPassword', 'socialLogin', 'getUserInfoForHai', 'resendEmailVerification', 'registerFirstStep', 'checkEmailVerification', 'registerLastStep', 'checkInviteLink', 'EmailVerified', 'sendPhoneOtp', 'checkUserDetail', 'sendSmsCode', 'SmsCodeVerification', 'intentionOption', 'ResendFaVerificationCode', 'onboardingScreens', 'storeUserDataFromOtherDb', 'betaBreakerClubUsers', 'haiChatHistory', 'createThreadIds']);
-        +
+        
         $this->auth = Auth::guard('api');
 
         $this->sns = $sns;
