d75d170532e1225cd921b0abc011c55753c8a65e
M Ahtasham <mahtasham060@gmail.com>
2025-10-29 17:17:38 +0500

    code update

--- clear.sh
+++ clear.sh
@@ -1,10 +1,9 @@
 echo "Deploy script started"
 
-# Install deps first so autoload & vendors are up to date
+# 1) Vendors first
 sudo -u www-data composer -n install --prefer-dist --no-progress --no-interaction
-sudo -u www-data composer -n dump-autoload -o
 
-# Clear all possible stale state
+# 2) Clear any stale state
 sudo -u www-data php artisan optimize:clear
 sudo -u www-data php artisan cache:clear
 sudo -u www-data php artisan config:clear
@@ -13,17 +12,8 @@ sudo -u www-data php artisan view:clear
 sudo -u www-data php artisan event:clear
 sudo -u www-data php artisan clear-compiled
 
-# DB changes
+# 3) DB changes (non-interactive)
 sudo -u www-data php artisan migrate --force
-
-# Rebuild caches (if desired for speed)
-sudo -u www-data php artisan config:cache
-sudo -u www-data php artisan route:cache
-sudo -u www-data php artisan view:cache
-sudo -u www-data php artisan event:cache
-
-# Optional feel-good :)
-sudo -u www-data php artisan inspire
 
 echo "Deploy script finished execution"
 # sudo -u www-data php artisan db:seed --class=CreditPlanSeeder
