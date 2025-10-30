0584028b5e104cb951f1350e511cdcff4be97c0f
M Ahtasham <mahtasham060@gmail.com>
2025-10-30 18:09:31 +0500

    blue ticket id check

--- app/Helpers/BlueHelper/BlueHelpers.php
+++ app/Helpers/BlueHelper/BlueHelpers.php
@@ -71,8 +71,6 @@ class BlueHelpers
         if (isset($responseData['data']['createTodo'])) {
             $ticket = $responseData['data']['createTodo'];
 
-            dd($ticket);
-            
             return [
                 'blue_ticket_id' => $ticket['id'] ?? null,
                 'blue_ticket_status' => $ticket['status'] ?? null,
