9b3c22c670f1146241748309651f6b49979a0609
M Ahtasham <mahtasham060@gmail.com>
2025-11-01 14:25:34 +0500

    plan added

--- resources/views/livewire/admin/user/all-user.blade.php
+++ resources/views/livewire/admin/user/all-user.blade.php
@@ -170,7 +170,13 @@
                             <select class="form-control input-form-style"
                                     onchange="changeUserPlan({{ $user['id'] }}, this.value)"
                                     style="background-color: #0F1535; border-radius: 12px;">
-                                <option value="premium_lifetime" {{ $user['plan'] === 'premium_lifetime' ? 'selected' : '' }}>
+                                @if($user['plan'] == null or $user['plan'] == 'freemium' or $user['plan'] == 'Freemium')
+                                    <option value="premium_lifetime" selected>Freemium</option>
+                                @elseif($user['plan'] == 'premium_monthly' or $user['plan'] == 'premium_yearly')
+                                    <option value="premium_lifetime" selected>Premium</option>
+                                @endif
+                                <option
+                                    value="premium_lifetime" {{ $user['plan'] === 'premium_lifetime' ? 'selected' : '' }}>
                                     Premium Lifetime
                                 </option>
                                 <option value="bb_onetime" {{ $user['plan'] === 'bb_onetime' ? 'selected' : '' }}>
