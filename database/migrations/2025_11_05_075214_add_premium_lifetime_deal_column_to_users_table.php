5675aebf85956d3d7885f8ae1ef8405c5be5d989
M Ahtasham <mahtasham060@gmail.com>
2025-11-05 17:19:42 +0500

    Update 2025_11_05_075214_add_premium_lifetime_deal_column_to_users_table.php

--- database/migrations/2025_11_05_075214_add_premium_lifetime_deal_column_to_users_table.php
+++ database/migrations/2025_11_05_075214_add_premium_lifetime_deal_column_to_users_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
 
-            $table->dropColumn('skip_premium_lifetime_deal');
+//            $table->dropColumn('skip_premium_lifetime_deal');
 
         });
     }
