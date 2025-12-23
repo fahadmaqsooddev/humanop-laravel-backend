af45665627f4a5d64bbecfb108c9624d8495b7f2
Muhammad Hamza <hamza.waheed00777@gmail.com>
2025-12-23 17:00:21 +0500

    lifetime coupon v0.2

--- app/Http/Controllers/Api/ClientController/Billing/BillingController.php
+++ app/Http/Controllers/Api/ClientController/Billing/BillingController.php
@@ -718,6 +718,7 @@ class BillingController extends Controller
 
             if ($coupon->type === 'bb_lifetime') {
                 $user->has_bb_onetime = true;
+                $user->plan = 'bb_onetime';
             }
 
             $user->billing_context = 'b2c';
