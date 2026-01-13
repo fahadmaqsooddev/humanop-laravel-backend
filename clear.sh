a7b9e0c08a302ff3682ca1789c2ae64974182d89
Muhammad Hamza <hamza.waheed00777@gmail.com>
2026-01-13 22:32:44 +0500

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,9 +1,13 @@
 echo "Deploy script started"
 
-# 1) Vendors first
+# Fix permissions
+sudo chown -R www-data:www-data .
+sudo chmod -R ug+rw storage bootstrap/cache
+
+# Vendors
 sudo -u www-data composer -n install --prefer-dist --no-progress --no-interaction
 
-# 2) Clear any stale state
+# Clear stale state
 sudo -u www-data php artisan optimize:clear
 sudo -u www-data php artisan cache:clear
 sudo -u www-data php artisan config:clear
@@ -12,16 +16,17 @@ sudo -u www-data php artisan view:clear
 sudo -u www-data php artisan event:clear
 sudo -u www-data php artisan clear-compiled
 
-# 3) DB changes (non-interactive)
+# DB changes
 sudo -u www-data php artisan migrate --force
 
-# 4) Warm caches to mirror production behavior
+# Warm up caches
 sudo -u www-data php artisan config:cache
 sudo -u www-data php artisan route:cache
 sudo -u www-data php artisan view:cache
 sudo -u www-data php artisan event:cache
 
-# 5) Optional: restart workers if you use queues
+# Restart queue workers
 sudo -u www-data php artisan queue:restart
 
 echo "Deploy script finished execution"
+
