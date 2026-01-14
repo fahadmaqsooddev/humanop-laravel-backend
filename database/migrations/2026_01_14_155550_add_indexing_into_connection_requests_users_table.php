252a2c120c14e3e68ca65a7cd2abebd3d813fb4c
Muhammad Hamza <hamza.waheed00777@gmail.com>
2026-01-14 21:08:09 +0500

    Update 2026_01_14_155550_add_indexing_into_connection_requests_users_table.php

--- database/migrations/2026_01_14_155550_add_indexing_into_connection_requests_users_table.php
+++ database/migrations/2026_01_14_155550_add_indexing_into_connection_requests_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIndexesToUserConnectionTables extends Migration
+class AddIndexingToConnectionRequestsAndUsersTable extends Migration
 {
     public function up()
     {
