f9f66e9836af28516c777a175d3fb51fa2e3f30d
Muhammad Hamza <hamza.waheed00777@gmail.com>
2026-02-12 16:46:31 +0500

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -30,7 +30,5 @@ sudo find bootstrap/cache -type d -exec chmod g+s {} \;
 # Restart queue workers
 sudo -u www-data php artisan queue:restart
 
-php artisan db:seed --class=FamilyMatrixConfigurationSeeder
-
 echo "Deploy script finished execution"
 exit 0
