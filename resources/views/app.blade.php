a1b41d14b11a14ad0c1758bb4c2bc0583880aebb
Fahad Maqsood <fahadmaqsood11383@gmail.com>
2026-02-16 14:08:49 +0500

    Solve issue of indexing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
     @else
         <html lang="en">
         @endif
+
         <head>
+            <meta name="robots" content="noindex, nofollow">
             <meta charset="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
