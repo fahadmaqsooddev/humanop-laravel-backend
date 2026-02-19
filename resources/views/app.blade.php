0db089a900fc4482cc4ee8821e77ed3d94580a68
Fahad Maqsood <fahadmaqsood11383@gmail.com>
2026-02-19 12:57:44 +0500

    remove space

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
     @else
         <html lang="en">
         @endif
-
         <head>
             <meta charset="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
