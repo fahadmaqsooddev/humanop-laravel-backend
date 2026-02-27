85f145002e7a328d92dabca2dfe840646d35ebf5
Fahad Maqsood <fahadmaqsood11383@gmail.com>
2026-02-27 14:33:03 +0500

    add migration also

---
+++ database/migrations/2026_02_27_091118_add_unread_index_to_messages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+
+            $table->index(['is_read', 'message_thread_id', 'sender_id'], 'messages_is_read_thread_sender_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropIndex('messages_is_read_thread_sender_idx');
+        });
+    }
+};
